f1afca1ac82f5ed8184dacdef3ad90693078501d
David <dvarrui@protonmail.com>
2018-12-06 14:37:29 +0000

    ROR 06 Creating Tag Model

--- messenger/db/schema.rb
+++ messenger/db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_12_06_141757) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
