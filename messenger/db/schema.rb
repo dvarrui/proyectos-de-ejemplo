be6832ead7d7484d26da7ad187dd1a5563510b6f
David <dvarrui@protonmail.com>
2018-12-09 14:24:03 +0000

    ROR 17 associations OK

--- messenger/db/schema.rb
+++ messenger/db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_015709) do
+ActiveRecord::Schema.define(version: 2018_12_09_135454) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages_tags", id: false, force: :cascade do |t|
+    t.integer "message_id", null: false
+    t.integer "tag_id", null: false
   end
 
   create_table "tags", force: :cascade do |t|
