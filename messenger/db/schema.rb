ffe7c53673fec63cdecedb2a37de900c1b386b80
David <dvarrui@protonmail.com>
2018-12-09 02:07:59 +0000

    N message model

--- messenger/db/schema.rb
+++ messenger/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_141757) do
+ActiveRecord::Schema.define(version: 2018_12_09_015709) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
