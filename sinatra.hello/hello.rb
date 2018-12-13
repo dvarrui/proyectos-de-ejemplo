bd6d528fc070aa4b6ba61015e711c34562765bce
David <dvarrui@protonmail.com>
2018-12-13 02:53:17 +0000

    U

--- sinatra.hello/hello.rb
+++ sinatra.hello/hello.rb
@@ -3,9 +3,16 @@ require "sinatra/base"
 class HelloWorld < Sinatra::Base
 
   get '/' do
-    "HelloWorld!"
+    erb :index
   end
 
+  get '/hello' do
+    erb :hello
+  end
+
+  get '/iam/:name' do
+    "Hello #{params['name']}!"
+  end
 end
 
 HelloWorld.run!
