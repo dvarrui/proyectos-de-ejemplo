87a1be168d72292117e62b4459dcd0a71d0f1a07
David <dvarrui@protonmail.com>
2019-01-12 22:58:50 +0000

    N

---
+++ iloveruby/parser/lib/ast.rb
@@ -0,0 +1,37 @@
+
+class Ast
+  attr_reader :data
+
+  def initialize(language)
+    @language = language
+  end
+
+  def create_with(lines)
+    @data = []
+    lines.each_with_index do |line, index|
+      tokens = line.split(" ")
+      types = types_of_these tokens
+      @data << { :linenumber => index, :tokens => tokens, :types => types}
+    end
+  end
+
+  def types_of_these(tokens)
+    types = []
+    tokens.each do |token|
+      types << @language.type_of(token)
+    end
+    types
+  end
+
+  def show
+    puts " Order | Tokens | Lines"
+    puts "-------+--------+------------------"
+    @data.each do |item|
+      print " %3d   |" % item[:linenumber].to_i
+      print " %3d    |" %item[:tokens].size
+      print " #{item[:tokens].join(' ')}"
+      print " (#{item[:types].join(' ')})\n"
+    end
+  end
+
+end
