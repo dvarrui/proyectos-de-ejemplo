cac01350e2bd236ecb3be13f70e406f7509ed949
David <dvarrui@protonmail.com>
2019-01-22 09:32:16 +0000

    U

--- iloveruby/factorial/factorial_recursivo.c
+++ iloveruby/factorial/factorial_recursivo.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 
-double factorial(double n)
-  double f;
-  if (n<=1) {
+int factorial(int n) {
+  int f;
+  if(n<=1) {
     return 1;
   }
-  f = n * factorial(n-1)
+  f = n * factorial(n-1);
   return f;
 }
 
 int main() {
+  int n;
   printf ("Escriba el número: ");
   scanf("%d",&n);
   printf ("El Factorial es: %d", factorial(n) );
