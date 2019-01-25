62fd81d4606079366645d50f35d3936ed7e30a90
David <dvarrui@protonmail.com>
2019-01-25 22:01:51 +0000

    U calculating max recursive calls

--- comparativa/ejemplos/factorial/factorial_iterativo.py
+++ comparativa/ejemplos/factorial/factorial_iterativo.py
@@ -1,15 +1,10 @@
-def recursive_factorial(n):
-    if n == 1:
-        return 1
-    return n * recursive_factorial(n - 1)
-
-
-def iterative_factorial(n):
+# iterative
+def factorial(n):
     result = 1
     for i in range(2, n+1):
         result *= i
     return result
 
-
-print(iterative_factorial(4))
-print(recursive_factorial(4))
+print("   3!=", factorial(3))
+print("  30!=", factorial(30))
+print("3000!=", factorial(3000))
--- comparativa/ejemplos/factorial/factorial_iterativo.rb
+++ comparativa/ejemplos/factorial/factorial_iterativo.rb
@@ -1,16 +1,11 @@
 #!/usr/bin/env ruby
 
 def factorial(n)
-  return 1 if n <= 1
   f=1
-  while n>0 do
-    f = f * n
-    n -= 1
-  end
+  (2..n).each { |i| f*=i }
   return f
 end
 
-puts "3! = #{factorial(3)}"
-puts "30! = #{factorial(30)}"
-puts "300! = #{factorial(300)}"
-puts "3000! = #{factorial(3000)}"
+puts "    3! = #{factorial(3)}"
+puts "   30! = #{factorial(30)}"
+puts " 3000! = #{factorial(3000)}"
