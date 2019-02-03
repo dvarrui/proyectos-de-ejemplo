e98335e1f856d186945e7b7a425d16bac168804b
David <dvarrui@protonmail.com>
2019-02-03 13:36:15 +0000

    U java factorial

--- comparativa/ejemplos/factorial/FactorialRecursivo.java
+++ comparativa/ejemplos/factorial/FactorialRecursivo.java
@@ -1,13 +1,13 @@
 import java.util.stream.IntStream;
 
 public class FactorialRecursivo {
-	
+
 	public static final long factorial(int numero) {
-		return (numero > 1) ? factorial(numero - 1) * numero : 1; 
+		return (numero > 1) ? factorial(numero - 1) * numero : 1;
 	}
-	
+
 	public static void main(String[] args) {
-		IntStream.rangeClosed(1, 10).forEach(n -> System.out.println(n + "! = " + factorial(n)));
+		IntStream.rangeClosed(10, 50).forEach(n -> System.out.println(n + "! = " + factorial(n)));
 	}
 
 }
