efadd62cfae7fdc40d67101cd094ab1a640f9f30
David <dvarrui@protonmail.com>
2019-02-21 23:20:01 +0000

    N

---
+++ comparativa/ejemplos/02-math/program4b.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+radio = ARGV[0].to_f || 1
+
+puts('Operaciones disponibles')
+puts('1 - Calcular el diámetro de la circunferencia')
+puts('2 - Calcular el perímetro de la circunferencia')
+puts('3 - Calcular el área del círculo')
+puts('4 - Salir')
+
+print 'Elige una opción del menú (1-4) '
+opcion = gets.to_i
+
+case opcion
+when 1 then
+  diametro = 2 * radio
+  puts("El diámetro de la circunferencia es de #{diametro}")
+when 2 then
+  perimetro = 2 * pi * radio
+  puts("El perímetro de la circunferencia es de #{perimetros}")
+when 3 then
+  area = pi * radio ** 2
+  puts("El área del círculo es de #{area}")
+when 4 then
+  puts('Has elegido salir de la aplicación')
+else
+  puts('¡ERROR! Sólo puedes elegir una opción entre 1 y 4')
+end
