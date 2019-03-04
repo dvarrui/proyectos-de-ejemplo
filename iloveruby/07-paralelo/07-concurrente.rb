3c70c63cfa454aba87ebbed8754a5e92c299969f
David <dvarrui@protonmail.com>
2019-03-04 18:01:54 +0000

    U

--- iloveruby/07-paralelo/07-concurrente.rb
+++ iloveruby/07-paralelo/07-concurrente.rb
@@ -3,7 +3,7 @@
 require_relative '07-concurrente-lib'
 
 inputs = [ 2, 4, 6, 8].shuffle
-action = lambda { |a,b| do_something(a,b)}
+action = lambda { |id,delay| do_something(id,delay)}
 
 begin_work inputs
 inputs.parallelize action
