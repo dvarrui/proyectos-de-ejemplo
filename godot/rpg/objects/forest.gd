041adfa6c5be602bdde850b5dd4cf1b392eea20c
David <dvarrui@protonmail.com>
2019-06-28 22:41:56 +0100

    N godot rpg under development

---
+++ godot/rpg/objects/forest.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var tree_class = load("res://objects/tree.tscn")
+var size = Vector2(100,100)
+export var items_count = 3 
+
+func _ready():
+	size = $shape.shape.get("extents")
+	for i in range(items_count):
+		add_tree()
+
+func add_tree():
+	var t = tree_class.instance()
+	t.position.x = rand_range( -size.x, size.x)
+	t.position.y = rand_range( -size.y, size.y)
+	add_child(t)
+	
---
+++ godot/rpg/objects/tree.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+# export (String) var sprite_name = "tree01"
+
+func _ready():
+	z_index = position.y
