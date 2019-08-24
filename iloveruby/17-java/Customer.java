f8072ff8b657c9ac156eeab9b98221166b2cc921
David <dvarrui@protonmail.com>
2019-08-24 16:49:02 +0100

    F

--- iloveruby/17-java/Customer.java
+++ iloveruby/17-java/Customer.java
@@ -1,14 +1,14 @@
 // Ejemplo de https://medium.com/@Nicholson85/method-overloading-in-ruby-22a169e704c7
 public class Customer {
-	public static void DisplayInfo(string firstName){
+	public static void DisplayInfo(String firstName){
 		Console.WriteLine("Their name is " + firstName);
 	}
 
-	public static void DisplayInfo(string firstName, string lastName){
+	public static void DisplayInfo(String firstName, String lastName){
 		Console.WriteLine("Their name is " + firstName + " " + lastName);
 	}
 
-	public static void DisplayInfo(string firstName, string lastName, string email){
+	public static void DisplayInfo(String firstName, String lastName, String email){
 		Console.WriteLine("Their name is " + firstName + " " + lastName + " and their email address is " + email);
 	}
 }
