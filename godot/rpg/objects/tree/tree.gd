9dd15e4eff9aa69d3a59b2d4063ce2bd40c55d99
David <dvarrui@protonmail.com>
2019-11-21 18:39:19 +0000

    U

--- godot/rpg/objects/tree/tree.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-
-# export (String) var sprite_name = "tree01"
-
-func _ready():
-	z_index = position.y
-	$info.set_info(z_index)
--- godot/rpg/partials/forest_dinamic_tree.gd
+++ godot/rpg/partials/forest_dinamic_tree.gd
@@ -1,17 +1,18 @@
 extends Area2D
 
-var tree_class = load("res://objects/tree/tree.tscn")
+var tree_class = load("res://actors/tree/tree.tscn")
 var size = Vector2(100,100)
-export var items_count = 3 
+export var ext_items_count = 3 
 
 func _ready():
 	size = $shape.shape.get("extents")
-	for i in range(items_count):
+	for i in range(ext_items_count):
 		add_tree()
 
 func add_tree():
 	var t = tree_class.instance()
 	t.position.x = rand_range( -size.x, size.x)
 	t.position.y = rand_range( -size.y, size.y)
+	t.ext_z = position.y + t.position.y
 	add_child(t)
 	
