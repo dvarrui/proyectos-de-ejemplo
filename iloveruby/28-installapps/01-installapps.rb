ee010244e1c6a635348702e463e0e2942c725d41
David Vargas <dvarrui@protonmail.com>
2019-12-07 19:31:40 +0000

    U

--- iloveruby/28-installapps/01-installapps.rb
+++ iloveruby/28-installapps/01-installapps.rb
@@ -1,4 +1,9 @@
-#!/usr/bin/env ruby 
+#!/usr/bin/env ruby
 
 puts "Executing #{$0}..."
 
+system('zypper refresh')
+system('zypper install -y tree')
+system('zypper install -y nmap')
+system('zypper install -y git')
+system('zypper install -y geany')
