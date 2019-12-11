45a0da810baeb2bdab5bb918c17a305d82a1ce4c
David <dvarrui@protonmail.com>
2019-12-11 19:07:26 +0000

    U

--- iloveruby/29-tour/lib/tour.rb
+++ iloveruby/29-tour/lib/tour.rb
@@ -4,8 +4,11 @@ require_relative 'tour/debug'
 
 class Tour
   def self.start(input)
-    puts "[INFO] Starting tour..."
-    data = Markdown.new(input.first).parse
+    if input.empty?
+      puts "Usage: tour PATH/TO/FILE.md"
+      exit 1
+    end
+    data = Markdown.new.parse(input.first)
     Debug.show_data(data)
   end
 end
--- iloveruby/29-tour/lib/tour/markdown.rb
+++ iloveruby/29-tour/lib/tour/markdown.rb
@@ -4,21 +4,19 @@ require 'pry-byebug'
 class Markdown
   attr_reader :data
 
-  def initialize(filepath)
-    @filepath = filepath
+  def initialize
     @data = { title: 'No title',
               slides: [] }
   end
 
-  def parse
-    puts "[INFO] Parsing #{@filepath}"
-    @lines = File.read(@filepath).split("\n")
-    @lines.delete('')
+  def parse(filepath)
+    lines = File.read(filepath).split("\n")
+    lines.delete('')
 
     current = nil
     i = 0
-    while (i<@lines.size)
-      line = @lines[i]
+    while (i<lines.size)
+      line = lines[i]
       if line.start_with?('# ')
         @data[:title] = line.gsub('# ', '')
       elsif line.start_with?('## ')
