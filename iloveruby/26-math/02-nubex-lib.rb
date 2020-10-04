136cf86e3dbddac303b4eafcc1921d784f69235f
dvarrui <dvarrui@protonmail.com>
2020-10-04 16:15:14 +0100

    U

--- iloveruby/26-math/02-nubex-lib.rb
+++ iloveruby/26-math/02-nubex-lib.rb
@@ -1,5 +1,7 @@
 
 class Nubex
+  attr_reader :data
+
   def initialize(base, exp)
     @data = { base => exp}
   end
@@ -12,7 +14,18 @@ class Nubex
 
   def to_s
     out = ''
-    @data.each { |key, value| out += "#{key}^#{value }"}
+    @data.sort.each { |item| out += "#{item[0]}^#{item[1]} "}
     out
+  end
+
+  def mul(other)
+    other.data.each do |key, value|
+      if @data[key].nil?
+        @data[key] = value
+      else
+        @data[key] += value
+        @data.delete(key) if @data[key].zero?
+      end
+    end
   end
 end
--- iloveruby/26-math/02-nubex.rb
+++ iloveruby/26-math/02-nubex.rb
@@ -7,3 +7,10 @@ b = Nubex.new(3,2)
 
 puts "#{a} = #{a.to_i}"
 puts "#{b} = #{b.to_i}"
+
+a.mul(b)
+puts "#{a} = #{a.to_i}"
+
+c = Nubex.new(3, -2)
+a.mul(c)
+puts "#{a} = #{a.to_i}"
