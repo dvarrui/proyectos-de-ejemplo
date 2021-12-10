55eb3e7bb2797fd1606d70d427299e1d03756e1d
dvarrui <dvarrui@protonmail.com>
2021-12-10 21:07:02 +0000

    U

--- charlas/rails/vagrant/install-rails.sh
+++ charlas/rails/vagrant/install-rails.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "[INFO] Install rails v2"
+echo "[INFO] Install rails into VM"
 
 echo "[INFO] Installing tools..."
-zypper --non-interactive in vim tree nmap git
+zypper --non-interactive --quiet in vim tree nmap git
 
 echo "[INFO] Installing rails..."
-zypper --non-interactive in ruby2.5-rubygem-rails-5.2
+zypper --non-interactive --quiet in ruby2.5-rubygem-rails-5.2
 echo "alias rails='rails.ruby2.5'" >> /home/vagrant/.alias
 
 echo "[INFO] Installing sqlite3..."
-zypper --non-interactive in sqlite3 sqlite3-devel
+zypper --non-interactive --quiet in sqlite3 sqlite3-devel
