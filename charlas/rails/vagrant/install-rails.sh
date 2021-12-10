cd47b942b28dbf9cf9b5a526dfe3526e48bbe481
dvarrui <dvarrui@protonmail.com>
2021-12-10 16:51:10 +0000

    U

--- charlas/rails/vagrant/install-rails.sh
+++ charlas/rails/vagrant/install-rails.sh
@@ -5,12 +5,8 @@ echo "[INFO] Installing tools..."
 zypper --non-interactive in vim tree nmap git
 
 echo "[INFO] Installing rails..."
-#zypper --non-interactive --quiet in gcc make kernel-devel ruby-devel
 zypper --non-interactive in ruby2.5-rubygem-rails-5.2
+echo "alias rails='rails.ruby2.5'" >> /home/vagrant/.alias
 
 echo "[INFO] Installing sqlite3..."
 zypper --non-interactive in sqlite3 sqlite3-devel
-#zypper --non-interactive --quiet in gcc gcc10-c++
-#ln -s /usr/bin/gcc-10 /usr/bin/g++
-#gem install mini_racer -v 0.4.0
-echo "alias rails='rails.ruby2.5'" >> /home/vagrant/.alias
