13c495aa2951f48d170ee2d8abf66ba10fbe9d18
David Vargas <dvarrui@protonmail.com>
2021-12-11 10:16:56 +0000

    U

--- charlas/rails/vagrant/install-rails.sh
+++ charlas/rails/vagrant/install-rails.sh
@@ -5,11 +5,10 @@ echo "[INFO] Installing tools..."
 zypper --non-interactive --quiet in vim tree git
 
 echo "[INFO] Installing rails..."
+zypper --non-interactive --quiet in gcc make ruby-devel
 zypper --non-interactive --quiet in ruby2.5-rubygem-rails-5.2
 echo "alias rails='rails.ruby2.5'" >> /home/vagrant/.alias
-
-zypper --non-interactive --quiet in gcc make kernel-devel ruby-devel
-# zypper --non-interactive --quiet in gcc gcc10-c++
+# zypper --non-interactive --quiet in gcc gcc10-c++ kernel-devel
 
 echo "[INFO] Installing sqlite3..."
 zypper --non-interactive --quiet in sqlite3 sqlite3-devel
