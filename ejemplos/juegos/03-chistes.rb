bf81504d2b610ed8f5683db51372c7971b3a3efa
David Vargas <dvarrui@protonmail.com>
2021-12-26 22:05:08 +0000

    N chiste

---
+++ ejemplos/juegos/03-chistes.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# chistes
+
+print('¿Qué sale del cruce entre un mono y un pato?')
+gets()
+puts("¡Un monopatín!\n\n")
+
+print("¿Porqué vuelan los pájaros pa\'l sur?")
+gets()
+puts("¡Porque caminando tardarían muchísimo!\n\n")
+
+print "¿En qué se parecen una familia, un bombero y un barco?"
+gets
+print "No sé... ¿en qué se parecen?"
+gets
+print "En que el bombero y el barco tienen casco."
+gets
+print '¿Y la familia?'
+puts ' -Bien, gracias.'
