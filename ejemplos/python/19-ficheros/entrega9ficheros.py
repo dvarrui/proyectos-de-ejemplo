ff6e85b47f302c678e4f1d6ab70d0229f77987d5
David Vargas <dvarrui@protonmail.com>
2021-12-29 17:07:40 +0000

    N entrega9

---
+++ ejemplos/python/19-ficheros/entrega9ficheros.py
@@ -0,0 +1,18 @@
+# ASGMT files
+# asigna valores iniciales
+filename = 'points.txt'
+
+# code
+f = open(filename)
+distances = []
+for line in f:
+    items = line.split()
+    x = int(items[0])
+    y = int(items[1])
+    distance = (x**2 + y**2) ** 0.5
+    distances.append(distance)
+
+print(min(distances))
+f.close()
+
+# min_distance = 4.123105625617661
