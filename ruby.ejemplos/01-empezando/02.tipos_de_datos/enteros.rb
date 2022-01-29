87c1d1eb2cb880d74b15d5c26a77e710af7d8c58
David Vargas <dvarrui@protonmail.com>
2022-01-29 06:03:50 +0000

    U

--- ruby.ejemplos/01-empezando/02.tipos_de_datos/enteros.rb
+++ ruby.ejemplos/01-empezando/02.tipos_de_datos/enteros.rb
@@ -6,3 +6,19 @@ class Integer
 end
 
 puts (0..13).map { |i| i.fac }.join(', ')
+
+
+❯ irb
+irb(main):001:0> 4.class
+=> Integer
+irb(main):002:0> 4.object_id
+=> 9
+irb(main):003:0> 3.class
+=> Integer
+irb(main):004:0> 3.object_id
+=> 7
+irb(main):005:0> 3 + 4
+=> 7
+irb(main):006:0> 3.+(4)
+=> 7
+irb(main):007:0>
