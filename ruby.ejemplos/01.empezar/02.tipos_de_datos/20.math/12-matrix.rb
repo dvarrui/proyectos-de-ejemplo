ae1ee6b474b3d188fed44f71681a32c578b1c851
dvarrui <dvarrui@protonmail.com>
2022-01-29 21:31:38 +0000

    U

--- ruby.ejemplos/01.empezar/02.tipos_de_datos/20.math/12-matrix.rb
+++ ruby.ejemplos/01.empezar/02.tipos_de_datos/20.math/12-matrix.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # http://rubylearning.com/blog/2013/04/04/ruby-matrix-the-forgotten-library/
 
-require_relative '02-matrix-lib'
+require_relative '12-matrix-lib'
 
 a = MyMatrix.new :size => 2, :value => 0
 puts "MyMatrix A #{a.to_s}"
