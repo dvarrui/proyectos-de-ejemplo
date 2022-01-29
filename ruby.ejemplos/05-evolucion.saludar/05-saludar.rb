6a70d9ab9118bea5efa42d7d6aaf1d9b101c544c
dvarrui <dvarrui@protonmail.com>
2022-01-29 20:38:07 +0000

    U

--- ruby.ejemplos/05-evolucion.saludar/05-saludar.rb
+++ ruby.ejemplos/05-evolucion.saludar/05-saludar.rb
@@ -8,11 +8,12 @@ if ARGV.size.zero?
     decir '¡Disculpe! ¿Le conozco?'
 elsif ARGV.size == 1
   preguntar_a nombre.capitalize
-elsif ARGV.size == 2
-  number = ARGV[1].to_i
-  number.times { decir hola_a(nombre) }
 else
   number = ARGV[1].to_i
-  decir 'ESO LOS SERAS TUUUU'
-  (1..number).each { |cadavez| decir hola_a(nombre, cadavez) }
+  if ARGV.size == 2
+    number.times { decir hola_a(nombre) }
+  else
+    decir 'ESO LO SERAS TU'
+    (1..number).each { |cadavez| decir hola_a(nombre, cadavez) }
+  end
 end
