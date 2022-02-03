f485cdc23c5347c0e8bf454f2fac95c6a3f72adf
David Vargas <dvarrui@protonmail.com>
2022-02-03 22:21:52 +0000

    U

--- ruby.ejemplos/51.paralelismo/01.threads/02-hilos-suma_y_resta.rb
+++ ruby.ejemplos/51.paralelismo/01.threads/02-hilos-suma_y_resta.rb
@@ -4,24 +4,30 @@
 x = 0
 max = 10
 inc = 2
+inc_sleep = 0.3
+inc_bonito = ' %2d' % inc
 dec = -1
+dec_sleep = 0.1
+dec_bonito = ' %2d' % dec
 
 # Hilo para decrementar
 Thread.new do
   while x > (max * -1)
     x += dec
-    puts "DEC[#{dec}]: Disminuyendo la x a #{x}\n"
-    sleep(0.1)
+    puts "DEC[#{dec_bonito}]: x=#{x}\n"
+    sleep(dec_sleep)
   end
-  puts "DEC[#{dec}]: x=#{x} Es muy pequeña. Gané!"
+  puts "DEC[#{dec_bonito}]: x=#{x} Es muy pequeña. Gana DEC!"
   exit 1
 end
 
 # Hilo para incrementar
 while x < (max)
   x += inc
-  puts "INC[#{inc}]: Aumentando a x a #{x}\n"
-  sleep(0.3)
+  puts "INC[#{inc_bonito}]: x=#{x}\n"
+  sleep(inc_sleep)
 end
+
+puts "INC[#{inc_bonito}]: x=#{x} Es muy grande. Gana INC!"
 
 exit 0
