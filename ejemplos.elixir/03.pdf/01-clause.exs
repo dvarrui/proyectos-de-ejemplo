07d5659432469a7c6441d774c94fcf34f51cab3e
David Vargas <dvarrui@protonmail.com>
2022-02-16 01:14:05 +0000

    [N] .

---
+++ ejemplos.elixir/03.pdf/01-clause.exs
@@ -0,0 +1,18 @@
+defmodule Math do
+
+  def is_even(num) when num === 1 do
+    false
+  end
+
+  def is_even(num) when num === 2 do
+    true
+  end
+
+  def is_odd(num) when num === 1 do
+    true
+  end
+
+  def is_odd(num) when num === 2 do
+    false
+  end
+end
