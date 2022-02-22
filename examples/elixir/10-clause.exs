390d7e48765bd95934ef5b639c39f7384cff0f56
David Vargas <dvarrui@protonmail.com>
2022-02-22 22:32:15 +0000

    [N] clause.

--- examples/elixir/10-clause.exs
+++ examples/elixir/10-clause.exs
@@ -1,22 +1,22 @@
 #!/usr/bin/env elixir
-#
-# https://elixir-lang.org/getting-started/alias-require-and-import.html
-#
 
-# Alias the module so it can be called as Bar instead of Foo.Bar
-# alias Foo.Bar, as: Bar
-#
-# Require the module in order to use its macros
-# require Foo
-#
-# Import functions from Foo so they can be called without the `Foo.` prefix
-# import Foo
-#
-# Invokes the custom code defined in Foo as an extension point
-# use Foo
+defmodule Clause do
+  def is_even(num) when num === 1 do
+    false
+  end
 
-# elixirc lib/10-clause-lib.exs
-require Clause
+  def is_even(num) when num === 2 do
+    true
+  end
+
+  def is_odd(num) when num === 1 do
+    true
+  end
+
+  def is_odd(num) when num === 2 do
+    false
+  end
+end
 
 IO.puts "[ info ] odd? 1, 2"
 IO.puts Clause.is_odd 1
---
+++ examples/elixir/12.clause.exs
@@ -0,0 +1,25 @@
+#!/usr/bin/env elixir
+#
+# https://elixir-lang.org/getting-started/alias-require-and-import.html
+#
+
+# Alias the module so it can be called as Bar instead of Foo.Bar
+# alias Foo.Bar, as: Bar
+#
+# Import functions from Foo so they can be called without the `Foo.` prefix
+# import Foo
+#
+# Invokes the custom code defined in Foo as an extension point
+# use Foo
+
+# IMPORTANTE!!!
+# elixirc lib/11-clause-lib.exs
+import Clause
+
+IO.puts "[ info ] odd? 1, 2"
+IO.puts is_odd 1
+IO.puts is_odd 2
+
+IO.puts "[ info ] even? 1, 2"
+IO.puts is_even 1
+IO.puts is_even 2
