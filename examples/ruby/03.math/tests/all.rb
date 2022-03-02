9b0b241a007088b1c4238485d30fae719b5724fa
David Vargas <dvarrui@protonmail.com>
2022-03-02 00:29:29 +0000

    [F] .

--- examples/ruby/03.math/tests/all.rb
+++ examples/ruby/03.math/tests/all.rb
@@ -10,3 +10,5 @@ require_relative 'monomio_add_test'
 require_relative 'monomio_subs_test'
 require_relative 'monomio_mul_test'
 require_relative 'monomio_div_test'
+
+require_relative 'polinomio_test'
--- examples/ruby/03.math/tests/polinomio_test.rb
+++ examples/ruby/03.math/tests/polinomio_test.rb
@@ -4,13 +4,13 @@ require 'minitest/autorun'
 require_relative '../lib/polinomio'
 
 class PoliomioTest < Minitest::Test
-  def setup    assert_equal (2=>7,0=>-1}), m[2].to_h
+  def setup
 
     @p = []
-    @p << Polinomio.new [ Monomio.new(5, 3), Monomio.new(-3, 0) ]
-    @p << Polinomio.new [ Monomio.new(6, 2), Monomio.new(-2, 1) ]
-    @p << Polinomio.new [ Monomio.new(7, 2), Monomio.new(-1, 0) ]
-    @p << Polinomio.new [ Monomio.new(8, 3), Monomio.new(-4, 2) ]
+    @p << Polinomio.new( [ Monomio.new(5, 3), Monomio.new(-3, 0) ])
+    @p << Polinomio.new( [ Monomio.new(6, 2), Monomio.new(-2, 1) ])
+    @p << Polinomio.new( [ Monomio.new(7, 2), Monomio.new(-1, 0) ])
+    @p << Polinomio.new( [ Monomio.new(8, 3), Monomio.new(-4, 2) ])
   end
 
   def test_to_s
@@ -23,9 +23,9 @@ class PoliomioTest < Minitest::Test
 
   def test_to_h
     p = @p
-    assert_equal ({3=>5,0=>-3}), p[0].to_h
-    assert_equal ({2=>3,1=>-2}), p[1].to_h
-    assert_equal (2=>7,0=>-1}), p[2].to_h
-    assert_equal (3=>8,2=>-4}), p[3].to_h
+    assert_equal ({ 3=>5, 0=>-3 }), p[0].to_h
+    assert_equal ({ 2=>3, 1=>-2 }), p[1].to_h
+    assert_equal ({ 2=>7, 0=>-1 }), p[2].to_h
+    assert_equal ({ 3=>8, 2=>-4 }), p[3].to_h
   end
-ena
+end
