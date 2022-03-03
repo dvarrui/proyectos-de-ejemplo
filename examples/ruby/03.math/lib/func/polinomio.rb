bb977b7b48e233de617fc58ddaf7822b8b19a879
David Vargas <dvarrui@protonmail.com>
2022-03-03 20:59:55 +0000

    [N] .

--- examples/ruby/03.math/lib/func/polinomio.rb
+++ examples/ruby/03.math/lib/func/polinomio.rb
@@ -1,6 +1,25 @@
 
+require_relative 'monomio'
+
 class Polinomio
 
-  def to_s(value)
+  def hash_to_a(polinomio_hash)
+    polinomio_array = []
+    xexps = polinomio_hash.keys.reverse
+    xexps.each do |xexp|
+      value = polinomio_hash[xpexp]
+      polinomio_array << [ value, xexp ]
+    end
+    polinomio_array
+  end
+
+  def self.array_to_s(polinomio_array)
+    polinomio_string = ''
+    polinomio_array.each do |monomio|
+      value = monomio[0]
+      xexp = monomio[1]
+      polinomio_string += Monomio.to_s(value, xexp)
+    end
+    polinomio_string
   end
 end
