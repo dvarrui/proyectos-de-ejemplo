83a230f72fec8feb9dc28ce0b4469481445220fa
David Vargas <dvarrui@protonmail.com>
2022-03-25 16:36:52 +0000

    [U] .

--- examples/ruby/13.tdd.filo/05.push_then_pop/tests/test_filo_pop.rb
+++ examples/ruby/13.tdd.filo/05.push_then_pop/tests/test_filo_pop.rb
@@ -10,7 +10,14 @@ class FiloPopTest < Test::Unit::TestCase
     assert_equal nil, filo.pop
   end
 
-  def test_push_element_and_pop_element
+  def test_push_1_element_and_pop_1_element
+    filo = Filo.new
+    filo.push(42)
+    assert_equal 42, filo.pop
+    assert_equal nil, filo.pop
+  end
+
+  def test_push_2_elements_and_pop_2_elements
     filo = Filo.new
     filo.push(42)
     assert_equal 42, filo.pop
