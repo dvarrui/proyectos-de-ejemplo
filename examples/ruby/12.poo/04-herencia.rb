a4f1819ddd5165c48643436f9a55a6df9b43b79f
David Vargas <dvarrui@protonmail.com>
2022-04-20 14:31:30 +0100

    [U] .

--- examples/ruby/12.poo/04-herencia.rb
+++ examples/ruby/12.poo/04-herencia.rb
@@ -1,34 +1,30 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
-class Humano
-  def identidad
-    print "soy una persona.\n"
+print "Ejecutando",$0,"\n"
+
+class Persona
+
+  def initialize(nombre)
+    @nombre = nombre
   end
-  def tarifa_tren(edad)
-    if edad < 12
-      print "tarifa reducida.\n"
-    else
-      print "tarifa normal. \n"
-    end
+
+  def saludar
+    "Hola, mi nombre es #{@nombre}."
+  end
+
+end
+
+david = Persona.new('David')
+
+puts david.saludar
+print "Soy de la clase " ,david.class.name,"\n"
+
+class Japones < Persona
+  def initialize
+   super('Unkown!')
   end
 end
 
-Humano.new.identidad
-
-class Estudiante<Humano
-  def identidad
-    print "soy un estudiante.\n"
-  end
-end
-
-Estudiante.new.identidad
-
-class Estudiante2<Humano
-  def identidad
-    super
-    print "también soy un estudiante.\n"
-  end
-end
-
-Estudiante2.new.identidad
-
+matz = Japones.new
+puts matz.saludar
+print "Soy de la clase ",matz.class.name,"\n"
