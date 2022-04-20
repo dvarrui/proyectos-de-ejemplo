c965a5f3c8733fb38103c392945cd06012a00ef6
David Vargas <dvarrui@protonmail.com>
2022-04-20 14:35:34 +0100

    [U] .

--- examples/ruby/12.poo/03-variables_instancia.rb
+++ examples/ruby/12.poo/03-variables_instancia.rb
@@ -1,32 +1,28 @@
+#!/usr/bin/env ruby 
 
 class InstTest
   def set_foo(n)
-          @foo = n
+    @foo = n
   end
+
   def set_bar(n)
-          @bar = n
+    @bar = n
   end
+
   def info
-     print self
-     print " foo=",@foo
-     print " bar=",@bar,"\n"
+    "#{self} foo=#{@foo} bar=#{@bar}"
   end
 end
 
 i = InstTest.new
-print i
-print "\n"
+puts i
 i.info
 
-puts defined? i
+puts " defined? " + defined? i
 
 i.set_foo(2)
-print i
-print "\n"
-i.info
+puts i.info
 
 i.set_bar(4)
-print i 
-print "\n"
-i.info
+puts i.info
 
