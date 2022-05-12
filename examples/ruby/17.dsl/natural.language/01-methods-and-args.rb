16e12cffbce6a26695c56383beb07377fecdebb0
David Vargas <dvarrui@protonmail.com>
2022-05-12 18:46:40 +0100

    [U]

--- examples/ruby/17.dsl/natural.language/01-methods-and-args.rb
+++ examples/ruby/17.dsl/natural.language/01-methods-and-args.rb
@@ -1,16 +1,10 @@
 #!/usr/bin/env ruby
 
-def m1(*)
-  puts "m1(*) => "
-end
 
-def m2(value)
+def m1(value)
   puts "m2(value) => value = #{value}"
 end
 
-def m3(*args)
-  puts "m3(*args) => args = #{args}"
-end
 
 puts "\n"
 puts "=> def m1(*) ..."
@@ -20,13 +14,17 @@ print "   m1 'a','b','c' => "
 m1 'a', 'b', 'c'
 
 puts "\n"
-puts "=> def m2(value) ..."
-print "   m2 'a' 'b' 'c' => "
-m2 'a' 'b' 'c'
-print "   m2 'a','b','c'"
+puts "=> def m1(value) ..."
+print "   m1 'a' 'b' 'c' => "
+m1 'a' 'b' 'c'
+print "   m1 'a','b','c'"
 puts "   Argument error"
 
 # m2 'a', 'b', 'c' # => Argument error
+
+def m2(*args)
+  puts "m2(*args) => args = #{args}"
+end
 
 puts "\n"
 puts "=> def m3(*args)"
@@ -36,3 +34,6 @@ print "   m3 'a', 'b', 'c' => "
 m3 'a', 'b', 'c'
 
 
+def m1(*)
+  puts "m1(*) => "
+end
