5880005d471d1e79ec952c4e57ccefbff334ef9d
David Vargas <dvarrui@protonmail.com>
2022-05-19 12:35:54 +0100

    [U] .

--- examples/ruby/17.dsl/natural.language.02/01-talk.rb
+++
@@ -1,2 +0,0 @@
-
-require_relative 'lib/actor'
--- examples/ruby/17.dsl/natural.language.02/lib/actor.rb
+++ examples/ruby/17.dsl/natural.language.02/lib/actor.rb
@@ -1,13 +1,17 @@
 
-require_relative 'history'
+require 'colorize'
 
 class Actor
+  attr_writer :talk
+  attr_reader :name
 
-  def initialize(name)
-    @name = name
+  def initialize(name, color)
+    @name = name.upcase
+    @color = color
   end
 
+  def say(text)
+    puts "[#{@name}] #{text}".colorize(@color)
+    @talk.add_sentence(@name, @text)
+  end
 end
-
-yo = Actor.new(:yo)
-tu = Actor.new(:tu)
