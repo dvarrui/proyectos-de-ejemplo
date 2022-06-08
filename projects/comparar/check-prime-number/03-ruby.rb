58f745b97c98752f4f9ed0b4777a1d0f01bd1d59
David Vargas <dvarrui@protonmail.com>
2022-06-08 23:49:21 +0100

    [U] .

--- projects/comparar/check-prime-number/03-ruby.rb
+++ projects/comparar/check-prime-number/03-ruby.rb
@@ -3,22 +3,20 @@
 num = 407
 
 # To take input from the user
-#num = int(input("Enter a number: "))
+# print "Enter a number: "
+# num = gets.to_i
 
-# prime numbers are greater than 1
-if num > 1
-  # check for factors
-  (2..(num-1)).each do |i|
-    if (num % i).zero?
-      puts "#{num} is NOT a prime number"
-      puts "#{i} times #{num/i} is #{num}"
-      break
-    else
-      puts "#{num} is a prime number"
-    end
+def divisor_of(number)
+  max = number - 1
+  (2..max).each  { |i| return i  if (number % i).zero? }
+  nil
+end
 
-# if input number is less than
-# or equal to 1, it is not prime
+divisor = divisor_of(num)
+
+if divisor
+  puts "#{num} is NOT a prime number"
+  puts "#{divisor} times #{num/divisor} is #{num}"
 else
-  puts #{num} is not a prime number"
+  puts "#{num} is a prime number"
 end
