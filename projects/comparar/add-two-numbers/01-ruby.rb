00ecfd2e7b20f8bebbb17fc1d80d0ffef19c1aaa
David Vargas <dvarrui@protonmail.com>
2022-06-08 22:43:23 +0100

    [N] .

---
+++ projects/comparar/add-two-numbers/01-ruby.rb
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+puts "The sum of #{num1} and #{num2} is #{sum}"
---
+++ projects/comparar/add-two-numbers/02-ruby.rb
@@ -0,0 +1,11 @@
+# Store input numbers
+print "Enter first number: "
+num1 = gets.to_f
+print "Enter second number: "
+num2 = gets.to_f
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+puts "The sum of #{num1} and #{num2} is #{sum}"
