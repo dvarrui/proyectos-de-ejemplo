af5dd600ebf6b2ae5d388cdb993dd91b1917058c
David Vargas <dvarrui@protonmail.com>
2022-06-11 06:59:14 +0100

    [U] .

--- projects/comparar/find-factorial-of-a-number/02-ruby.rb
+++ projects/comparar/find-factorial-of-a-number/02-ruby.rb
@@ -1,21 +1,15 @@
-# program to find the factorial of a number provided by the user
-# using recursion
+# find the factorial of a number provided by the user using recursion
 
 def factorial(x)
-  # This is a recursive function
-  # to find the factorial of an integer
-
   return 1 if x == 1
-  # recursive call to the function
+
   return (x * factorial(x-1))
 end
 
-# change the value for a different result
 num = 7
 
 # to take input from the user
-# num = int(input("Enter a number: "))
+# print "Enter a number: "; num = gets.tp_i
 
-# call the factorial function
 result = factorial(num)
 puts "The factorial of #{num} is #{result}"
