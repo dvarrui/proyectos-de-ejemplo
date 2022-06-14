23b94748316be392e7b30424e9f47a6f0bd0e45d
David Vargas <dvarrui@protonmail.com>
2022-06-14 12:44:48 +0100

    [U] .

--- examples/ruby/41.gems/tty.toolkit/02-professbar.rb
+++ examples/ruby/41.gems/tty.toolkit/02-professbar.rb
@@ -10,3 +10,8 @@ bar = TTY::ProgressBar.new("downloading [:bar]", total: 30)
   sleep(0.1)
   bar.advance  # by default increases by 1
 end
+
+#bars = TTY::ProgressBar::Multi.new("main [:bar] :percent")
+
+#bar1 = bars.register("one [:bar] :percent", total: 15)
+#bar2 = bars.register("two [:bar] :percent", total: 15)
