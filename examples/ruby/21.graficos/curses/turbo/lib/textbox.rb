f1bc5e94270da39d41aa923b81008a08f94e4ce2
David Vargas <dvarrui@protonmail.com>
2022-06-16 15:28:50 +0100

    [U] .

--- examples/ruby/21.graficos/curses/turbo/lib/textbox.rb
+++ examples/ruby/21.graficos/curses/turbo/lib/textbox.rb
@@ -3,12 +3,12 @@ require 'curses'
 
 class TextBox
   attr_reader :data
-  
+
   def initialize(x:, y:, w:, h:)
     @box_position = { x: x, y: y }
     @box_size = { w: w, h: h }
     @prompt = { x: 0, y: 0 }
-    @data = [ '' ]
+    @data = [ '', '' ]
   end
 
   def run
@@ -18,22 +18,53 @@ class TextBox
       when "q"
         return
       when Curses::KEY_DOWN
+        ch = ''
+        next if @prompt[:y] > @box_size[:h]
         @prompt[:y] += 1
+        @prompt[:x] = 1
+        @data << '' if @data[@prompt[:y]].nil?
+      when Curses::KEY_UP
+        ch = ''
+        next if @prompt[:y].zero?
+        @prompt[:y] -= 1
+        @prompt[:x] = 1
+      when Curses::KEY_BACKSPACE
+        ch = ''
+        next if @prompt[:x].zero?
+        @prompt[:x] -= 1
+      when Curses::KEY_LEFT
+        ch = ''
+        next if @prompt[:x].zero?
+        @prompt[:x] -= 1
+      when Curses::KEY_RIGHT
+        ch = ''
+        next if @prompt[:x] < @box_size[:w]
+        @prompt[:x] += 1
       else
+        next if @prompt[:x] > @box_size[:w]
         @data[@prompt[:y]] << ch
-        pos = global_position
-        Curses.setpos(pos[:y], pos[:x]);
-        Curses.addstr(ch)
         @prompt[:x] += 1
       end
+      pos = global_position
+      Curses.setpos(pos[:y], pos[:x]);
+      Curses.addstr(ch.to_s)
+    end
+  end
+
+  def debug
+    @data.each_with_index do |line, index|
+      puts " #{index} : #{line}"
     end
   end
 
   private
 
   def global_position
-    { x: @box_position[:x] + @prompt[:x],
-      y: @box_position[:y] + @prompt[:y] }
+    pos = { x: @box_position[:x] + @prompt[:x],
+            y: @box_position[:y] + @prompt[:y] }
+    Curses.setpos(3,3)
+    Curses.addstr("(#{pos[:x]}, #{pos[:y]})")
+    pos
   end
 
 end
--- examples/ruby/21.graficos/curses/turbo/lib/turbo.rb
+++ examples/ruby/21.graficos/curses/turbo/lib/turbo.rb
@@ -8,7 +8,12 @@ module Turbo
   def self.init
     init_screen
     crmode
+    Curses.cbreak
     noecho
+    Curses.stdscr.keypad = true
+    at_exit do
+      Curses.close_screen
+    end
     stdscr.box('|', "-")
     setpos(0,3); addstr(" TURBO (v0.1.0) ")
     setpos(24,3); addstr(" <Press 'q' to quit> ")
@@ -17,10 +22,10 @@ module Turbo
   def self.run
     init
 
-    textbox = TextBox.new(x:5, y:5, w:10, h:10)
+    textbox = TextBox.new(x:5, y:5, w:9, h:9)
     textbox.run
     close
-    puts textbox.data
+    textbox.debug
   end
 
   def self.close
