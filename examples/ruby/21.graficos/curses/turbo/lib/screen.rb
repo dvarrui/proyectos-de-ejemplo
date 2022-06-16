5fa103706eb4ae3ad08a65c9a30be6fd7bf7c949
David Vargas <dvarrui@protonmail.com>
2022-06-16 11:01:00 +0100

    [U] .

---
+++ examples/ruby/21.graficos/curses/turbo/lib/screen.rb
@@ -0,0 +1,36 @@
+
+require "curses"
+
+module Screen
+  extend Curses
+
+  def self.init
+    init_screen
+    crmode
+    noecho
+    stdscr.box('|', "-")
+    setpos(0,3); addstr(" TURBO (v0.1.0) ")
+    setpos(24,3); addstr(" <Press 'q' to quit> ")
+  end 
+
+  def self.run
+    init 
+
+    @pos = { x:10, y:10 }
+    loop do
+      c = get_char
+      case c
+      when "q"
+        return
+      when "\e" # ESC
+        return
+      else
+        setpos(@pos[:y], @pos[:x]); addstr(c)
+        @pos[:x] += 1
+      end
+    end
+    close_screen
+  end
+end 
+
+
--- examples/ruby/21.graficos/curses/turbo/turbo.rb
+++ examples/ruby/21.graficos/curses/turbo/turbo.rb
@@ -1,27 +1,6 @@
 #!/usr/bin/env ruby
 
+require_relative "lib/screen"
 
-require "curses"
-include Curses
+Screen.run
 
-init_screen
-crmode
-noecho
-stdscr.box('|', "-")
-setpos(0,3)
-addstr(" TURBO (v0.1.0) ")
-
-setpos(24,3)
-addstr(" <Press 'q' to quit> ")
-
-loop do
-  c = get_char
-  case c
-  when "q"
-    return
-  when "\e" # ESC
-    return
-  end
-end
-
-close_screen
