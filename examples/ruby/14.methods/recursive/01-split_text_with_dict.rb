22f82ef36903ea163de600fc2d7881b08d52a826
David Vargas <dvarrui@protonmail.com>
2022-07-20 18:00:11 +0100

    [U] .

--- examples/ruby/14.methods/recursive/01-split_text_with_dict.rb
+++ examples/ruby/14.methods/recursive/01-split_text_with_dict.rb
@@ -1,20 +1,23 @@
 #!/usr/bin/env ruby
 
-def split_text_with_dict(text, dict, prefix=[])
-  return prefix if text.empty?
-
+def split_text_with_dict(text, dict, splited=[])
   solutions = []
-  puts "[DEBUG.1] text=#{text}, prefix=#{prefix.to_s}, solutions=#{solutions.to_s}"
+  #puts "[DEBUG.1] text=#{text}, splited=#{splited.to_s}, solutions=#{solutions.to_s}"
   dict.each do |word|
     if text.start_with? word
       new_text = text.delete_prefix word
-      new_prefix = prefix << word
-      sols =  split_text_with_dict(new_text.dup, dict, new_prefix.dup)
-      sols.each { |s| solutions << s }
+      new_splited = splited.dup<< word
+      if new_text.empty?
+        solutions << new_splited
+      else
+        sols = split_text_with_dict(new_text, dict, new_splited)
+        sols.each { |s| solutions << s } 
+        #puts "[DEBUG.2] solutions=#{solutions} sols=#{sols}"
+      end
     end
   end
 
-  puts "[DEBUG.2] text=#{text}, prefix=#{prefix.to_s}, solutions=#{solutions.to_s}"
+  #puts "[DEBUG.3] text=#{text}, splited=#{splited.to_s}, solutions=#{solutions.to_s}"
   return solutions
 end
 
--- examples/ruby/14.methods/recursive/01-test.rb
+++ examples/ruby/14.methods/recursive/01-test.rb
@@ -5,21 +5,28 @@ require_relative '01-split_text_with_dict'
 
 class SplitTextWithDictTest < Test::Unit::TestCase
 
-  def test_1
+  def test_1_one_solution
     text = "abc"
     dict = [ "a", "b", "c" ]
     assert_equal [ "a", "b", "c" ], split_text_with_dict(text, dict)[0]
   end
 
-  def _test_2
+  def test_2_no_solution
     text = "abd"
     dict = [ "a", "b", "c" ]
-    assert_equal [], split_text_with_dict(text, dict)[0]
+    assert_equal nil, split_text_with_dict(text, dict)[0]
   end
 
-  def _test_3
+  def test_3_one_solution
     text = "abc"
     dict = [ "a", "bc", "c" ]
     assert_equal [ "a", "bc" ], split_text_with_dict(text, dict)[0]
+  end
+  
+  def test_4_two_solutions
+    text = "abc"
+    dict = [ "a", "ab", "bc", "c" ]
+    assert_equal [ "a", "bc" ], split_text_with_dict(text, dict)[0]
+    assert_equal [ "ab", "c" ], split_text_with_dict(text, dict)[1]
   end
 end
