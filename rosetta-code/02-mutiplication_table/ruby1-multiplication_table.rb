ade08dfe8306d75be422a1a3b6ecd5ee2d7ede9e
David Vargas <dvarrui@protonmail.com>
2022-08-14 11:07:15 +0100

    [U] .

--- rosetta-code/02-mutiplication_table/ruby1-multiplication_table.rb
+++ rosetta-code/02-mutiplication_table/ruby1-multiplication_table.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
+number = ARGV.first.to_i || 10
+
 def multiplication_table(number)
   1.upto(10) do |i|
     puts " %2d * %2d = %3d" % [ i, number, i*number ]
   end
 end
 
-multiplication_table 10
+multiplication_table number
--- rosetta-code/02-mutiplication_table/rust1-multiplication_table.rs
+++ rosetta-code/02-mutiplication_table/rust1-multiplication_table.rs
@@ -1,9 +1,18 @@
-// compile: rustc -o a.out 
+// compile: rustc -o a.out
+use std::io;
 
-fn main() {
-    let number: i32 = 10;
-
+fn multiplication_table(number :i32) {
     for i in 1..11 {
         print!(" {:2} * {:2} = {:3}\n", i, number, i*number)
     }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read from stdin");
+
+    let number: i32 = input.trim().parse().expect("invalid input");
+    multiplication_table(number);
 }
