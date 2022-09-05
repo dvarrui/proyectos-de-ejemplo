fea3f8a976d8e020035e4ef5442d7509ff04fbb0
David Vargas <dvarrui@protonmail.com>
2022-09-05 23:43:38 +0100

    [N] visualize_ruby examples.

---
+++ examples/ruby/41.gems/visualize_ruby/01-example.rb
@@ -0,0 +1,13 @@
+require "visualize_ruby"
+
+ruby_code = <<~RUBY
+  if hungry?
+    eat
+  else
+    work
+  end
+RUBY
+
+results = VisualizeRuby::Builder.new(ruby_code: ruby_code).build
+VisualizeRuby::Graphviz.new(results).to_graph(path: "example.png")
+
