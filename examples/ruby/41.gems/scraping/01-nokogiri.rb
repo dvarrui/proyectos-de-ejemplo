dd08f2497d66ce6069292a56a2f78ee2877014a3
David Vargas <dvarrui@protonmail.com>
2022-09-15 08:30:00 +0100

    [N] .

---
+++ examples/ruby/41.gems/scraping/01-nokogiri.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require "nokogiri"
+
+html = <<EOF
+<html>
+  <header>Esto es el header</header>
+  <body>
+    <h1>Este es el body</h1>
+    <div id="obiwan" class="jedi">Obiwan Kenobi</div>
+    <div>
+      <p>R2D2</p>
+    </div>
+    <p>Ejemplo de Nokogiri</p>
+  </body>
+</html>
+EOF
+
+doc = Nokogiri::HTML.parse(html)
+
+puts "=== Todas las etiquetas div ==="
+puts doc.css("div")
+
+puts "\n=== Todas las etiquetas div p ==="
+puts doc.css("div p")
+
+puts "\n=== Localizar class ==="
+items = doc.css("div")
+puts items.select { |i| i["class"] == "jedi" }
+
