edacf627e237ad87b9fd1c538a283918dee09292
David Vargas <dvarrui@protonmail.com>
2022-09-15 09:26:39 +0100

    [U] .

--- examples/ruby/41.gems/nokogiri/02-div_p_class.rb
+++ examples/ruby/41.gems/nokogiri/02-div_p_class.rb
@@ -7,9 +7,10 @@ html = <<EOF
   <header>Esto es el header</header>
   <body>
     <h1>Este es el body</h1>
-    <div id="obiwan" class="jedi">Obiwan Kenobi</div>
-    <div>
+    <div id="obiwan">Obiwan Kenobi</div>
+    <div class="droides">
       <p>R2D2</p>
+      <p>C3PO</p>
     </div>
     <p>Ejemplo de Nokogiri</p>
   </body>
@@ -23,9 +24,15 @@ puts doc.css("div")
 
 puts "\n=== Todas las etiquetas div p ==="
 puts doc.css("div p")
+
+puts "\n=== Texto de las etiquetas div p ==="
 doc.css("div p").each { puts "p.text = #{_1.text}" }
 
-puts "\n=== Localizar class ==="
+puts "\n=== Localizar div id obiwan ==="
 items = doc.css("div")
-puts items.select { |i| i["class"] == "jedi" }
+puts items.select { |i| i["id"] == "obiwan" }
+
+puts "\n=== Localizar div class droides ==="
+items = doc.css("div")
+puts items.select { |i| i["class"] == "droides" }
 
