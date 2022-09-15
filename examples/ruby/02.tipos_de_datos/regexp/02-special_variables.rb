55144fe55198bc86299bec928209114b02151e19
David Vargas <dvarrui@protonmail.com>
2022-09-15 09:45:16 +0100

    [U] .

--- examples/ruby/02.tipos_de_datos/regexp/02-special_variables.rb
+++ examples/ruby/02.tipos_de_datos/regexp/02-special_variables.rb
@@ -3,3 +3,7 @@
 puts "Programar en Ruby es divertido" =~ /Ruby/
 puts "#{$`}<<#{$&}>>#{$'}"
 
+text =  "Programar en Ruby es divertido"
+filter = /Ruby/
+result = filter.match(text)
+puts "#{result.pre_match}<<#{result.match 0}>>#{result.post_match}"
