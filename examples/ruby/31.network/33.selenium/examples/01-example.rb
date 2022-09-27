f68dae347a5c4ccef75e195e7c6993cf5f75c1db
David Vargas <dvarrui@protonmail.com>
2022-09-27 11:10:51 +0100

    [U] .

--- examples/ruby/31.network/33.selenium/examples/01-example.rb
+++ examples/ruby/31.network/33.selenium/examples/01-example.rb
@@ -3,7 +3,13 @@
 
 require "selenium-webdriver" # set webdriver gem for Selenium
 require "rubygems"
-driver = Selenium::WebDriver.for :chrome # driver object creation
+
+puts "Step 1/3"
+driver = Selenium::WebDriver.for :firefox # driver object creation
+
+puts "Step 2/3"
 driver.navigate.to "https://www.tutorialspoint.com/index.htm" # launch browser
+
+puts "Step 3/3"
 driver.close # close browser
 
