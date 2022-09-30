f37b4e29dcfa3f341bd1e71d1d96c0f9dc2b1e63
David Vargas <dvarrui@protonmail.com>
2022-09-30 10:32:01 +0100

    [N] .

---
+++ examples/ruby/31.network/33.selenium/04-aldi-38260.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require "selenium-webdriver"
+require "debug"
+
+def pause
+  puts "(Push enter to continue)"
+  gets
+end
+
+def click_button(text, driver)
+  driver.find_elements(:tag_name, 'button').each do |button|
+    if button.text == text
+      puts "    [Window] #{driver.title}"
+      puts "==> click #{button.text}"
+      button.click
+      return true
+    end
+    # puts "    * button... #{button.text}" unless button.text.size.zero?
+  end
+
+  driver.quit
+  puts "[ERROR] #{text} button not found!"
+  exit 1
+end
+
+URL = "https://www.aldi.es/supermercados/encuentra-tu-supermercado.html"
+CODE = "38260"
+
+driver = Selenium::WebDriver.for :firefox
+driver.get URL
+driver.manage.timeouts.implicit_wait = 30
+
+click_button("SALTAR", driver)
+
+driver.navigate.refresh
+driver.find_element(name: 'Zip').send_keys CODE, :return
+puts "==> send_keys Zip"
+
+handles = driver.window_handles
+driver.switch_to.window(handles[1])
+puts "    [Window] #{driver.title}"
+click_button("Continuar", driver)
+
+pause
+
+driver.quit
