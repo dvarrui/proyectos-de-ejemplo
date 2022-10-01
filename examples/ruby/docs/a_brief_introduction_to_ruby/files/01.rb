fa2a7de3759a31171d03dbeb9a4122caf3538525
dvarrui <dvarrui@protonmail.com>
2022-10-01 13:03:57 +0100

    [D] .

--- examples/ruby/docs/a_brief_introduction_to_ruby/files/01.rb
+++
@@ -1,9 +0,0 @@
-puts ("This is Ruby!")
-puts "Hello World!!!"
-print "Mark "
-print "Robert "
-print "Mahoney "
-puts
-
-#oh yeah, a '#' starts a single line comment
-#oh, oh, and there are no semicolons ;)
--- examples/ruby/docs/metaprog/README.md
+++
@@ -1,6 +0,0 @@
-
-
-# Metaprogramación
-
-* Enlaces de interés: https://www.halcyon.hr/posts/exploring-metaprogramming-in-ruby
-* [Metaprogración en Ruby - Askay's blog](docs/metaprogram_askay_blog.md)
