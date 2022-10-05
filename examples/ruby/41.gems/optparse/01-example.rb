517400db97383c7210c539236e3970e1e8a0d124
David Vargas <dvarrui@protonmail.com>
2022-10-05 14:38:15 +0100

    [N] .

---
+++ examples/ruby/41.gems/optparse/01-example.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+end.parse!
+
+p options
+p ARGV
+
