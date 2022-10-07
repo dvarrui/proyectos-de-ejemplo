4b90563e20d03d9f7564fb6cd1f717d6db8e3167
David Vargas <dvarrui@protonmail.com>
2022-10-07 13:54:56 +0100

    [N] airbnb selenium scrapping.

---
+++ examples/ruby/31.network/42.selenium/06-airbnb.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require_relative "06-airbnb-lib"
+require 'debug'
+
+URL = "https://www.airbnb.es"
+
+browser = Airbnb.new(URL)
+# browser.click_button("Vale", debug: true)
+browser.pause
+browser.click_button_attr("class", "f19g2zq0 dir dir-ltr", debug: true)
+browser.filter_destino("Canarias, España")
+browser.pause
