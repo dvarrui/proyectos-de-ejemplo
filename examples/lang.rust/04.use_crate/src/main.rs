a084474c2f5264411d12baf828c4544353383d70
dvarrui <dvarrui@protonmail.com>
2022-10-08 22:41:14 +0100

    [N] using crate ferris_say.

---
+++ examples/lang.rust/04.use_crate/src/main.rs
@@ -0,0 +1,13 @@
+extern crate ferris_says;
+
+use ferris_says::say;
+use std::io::{ stdout, BufWriter };
+
+fn main() {
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout());
+    say(out, width, &mut writer).unwrap();
+}
+
