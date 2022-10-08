8a0187fdcf8c5683130323136810dd3edbf5edc1
dvarrui <dvarrui@protonmail.com>
2022-10-08 15:25:34 +0100

    [U] iter.

--- examples/lang.rust/03.array/src/main.rs
+++ examples/lang.rust/03.array/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
     let values = [2,5,7,1,0,9];
 
+    println!("iter: {:?}", values.iter());
     for value in values.iter() {
     	println!("Value: {}", value);
     }
+    println!("iter: {:?}", values.iter());
 }
