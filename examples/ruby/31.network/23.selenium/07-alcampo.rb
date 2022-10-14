db4743fa903bc48ea121e74492f2c79a8fb64429
David Vargas <dvarrui@protonmail.com>
2022-10-14 12:12:43 +0100

    [R] .

--- examples/ruby/31.network/23.selenium/07-alcampo.rb
+++ examples/ruby/31.network/23.selenium/07-alcampo.rb
@@ -10,12 +10,13 @@ puts "[Title] #{browser.driver.title}"
 
 driver = browser.driver
 links = driver.find_elements(:tag_name, "a").map do |a| 
-  href = a.attribute("href")
-  unless /empresa\/tienda\//.match(href).nil?
-    href
-  else
-    nil
-  end
+  # href = a.attribute("href")
+  # unless /empresa\/tienda\//.match(href).nil?
+  system("echo '#{href}' >> alcampo.log")
+  href
+  #else
+  #  nil
+  #end
 end
 links.compact!
 puts links
