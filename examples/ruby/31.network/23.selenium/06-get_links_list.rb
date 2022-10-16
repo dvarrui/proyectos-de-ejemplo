148e5ecc5c9950be54d6042c17482ed4b853ff47
David Vargas <dvarrui@protonmail.com>
2022-10-16 23:18:14 +0100

    [U] .

--- examples/ruby/31.network/23.selenium/06-get_links_list.rb
+++ examples/ruby/31.network/23.selenium/06-get_links_list.rb
@@ -6,11 +6,11 @@ require 'debug'
 
 list = [
   {
-    filename: "airbnb.search.html",
+    filename: "links.airbnb",
     url: "https://www.airbnb.es/s/canaras/homes?tab_id=home_tab&search_type=search_query&refinement_paths%5B%5D=%2Fhomes&query=Canary%20Islands%2C%20Spain&flexible_trip_lengths%5B%5D=one_week&price_filter_input_type=0&price_filter_num_nights=5&pagination_search=true&cursor=eyJzZWN0aW9uX29mZnNldCI6MCwiaXRlbXNfb2Zmc2V0IjowLCJ2ZXJzaW9uIjoxfQ%3D%3D"
   },
   {
-    filename: "alcampo.search.html",
+    filename: "links.alcampo",
     url: "https://www.alcampo.es/empresa/tiendas"
   }
 ]
@@ -20,11 +20,13 @@ list.each do |web|
   driver = browser.driver
 
   puts "[Title] #{driver.title}"
-  content = driver.page_source
 
-  File.open(web[:filename],"w") do |line|
-    line.puts content
+  file = File.open(web[:filename],"w")
+  driver.find_elements(:tag_name, "a").map do |a|
+    href = a.attribute("href")
+    file.puts href
   end
+  file.close
 
   browser.quit
 end
