5733d12fe4d0e0ddb95d6820d96ff42be7bd1da9
David Vargas <dvarrui@protonmail.com>
2022-10-26 15:05:28 +0100

    [N] .

---
+++ examples/python/05-main/import/libgreet.py
@@ -0,0 +1,11 @@
+def greet1(name):
+    print("Hello", name)
+
+def greet2(name):
+    print("Hello " + name)
+
+def greet3(name):
+    print("Hello %s" % name)
+
+def greet4(name):
+    print(f"Hello {name}")
---
+++ examples/python/05-main/import/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from libgreet import *
+
+greet1("Vader")
+greet2("Leia")
+greet3("Obiwan")
+greet4("Yoda")
