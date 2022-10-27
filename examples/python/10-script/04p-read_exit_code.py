1aac2eec4cf8959066374b39db45a7223bc3259f
Fran Vargas <francisco.vargas.ruiz@gmail.com>
2022-10-27 10:10:21 +0100

    U getting exit code using os.system

--- examples/python/10-script/04p-read_exit_code.py
+++ examples/python/10-script/04p-read_exit_code.py
@@ -7,3 +7,8 @@ print( 'exit status code:', p.returncode )
 
 p = subprocess.run( [ 'ping', '-c', '1', '192.168.16.16' ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL )
 print( 'exit status code:', p.returncode )
+
+import os
+
+retcode = os.system('ping -c 1 8.8.8.8 > /dev/null 2> /dev/null')
+print( 'exit status code:', retcode)
