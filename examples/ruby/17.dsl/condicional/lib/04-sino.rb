0397cb4daafc573051094296777750f792e63615
David Vargas <dvarrui@protonmail.com>
2022-10-27 10:44:17 +0100

    [N] dsl spanish conditional.

---
+++ examples/ruby/17.dsl/condicional/lib/04-sino.rb
@@ -0,0 +1,26 @@
+
+def si(&block)
+  result = block.call
+  $SI =
+  if result
+    $SI = true
+  else
+    $SI = false
+  end
+  $SI
+end
+
+def entonces(&block)
+  if $SI == true
+    block.call
+    $SI = nil
+  end
+end
+
+def sino(&block)
+  if $SI == false
+    block.call
+    $SI = nil
+  end
+end
+
