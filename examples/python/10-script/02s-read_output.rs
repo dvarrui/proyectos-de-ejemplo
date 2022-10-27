3860822a15b05dfe0e7cd8af6fc1c9a56fd3b07e
David Vargas <dvarrui@protonmail.com>
2022-10-27 07:51:34 +0100

    [F] .

--- examples/python/10-script/02s-read_output.rs
+++ examples/python/10-script/02s-read_output.rs
@@ -4,5 +4,6 @@ fn main() {
     let output = Command::new("date")
                          .output()
                          .unwrap();
-    println!("La fecha actual es: {:?}", output);
+    let text = String::from_utf8_lossy(&output.stdout);
+    println!("La fecha actual es: {}", text);
 }
