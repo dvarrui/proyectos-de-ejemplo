0dd1381d40f083eda7e710ae33122d00ba36c9e0
David Vargas <dvarrui@protonmail.com>
2022-10-27 11:08:29 +0100

    [U] .

--- examples/ruby/17.dsl/condicional/04-sino.rb
+++ examples/ruby/17.dsl/condicional/04-sino.rb
@@ -6,3 +6,13 @@ number = 10
 si { number >= 18 }
   entonces { puts "Adulto" }
   sino { puts "Menor" }
+
+
+number = 52
+
+si { number >= 18 }
+entonces {
+  puts "Adulto"
+}; sino {
+  puts "Menor"
+}
