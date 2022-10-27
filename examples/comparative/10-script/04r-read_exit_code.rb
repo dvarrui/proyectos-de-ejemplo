0356dfcd12872721fcde87ef8909386534186743
David Vargas <dvarrui@protonmail.com>
2022-10-27 10:18:12 +0100

    [U] .

--- examples/comparative/10-script/04r-read_exit_code.rb
+++ examples/comparative/10-script/04r-read_exit_code.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-system("ping 192.168.1.1 -c 1 > /dev/null")
+puts system("ping 192.168.1.1 -c 1 > /dev/null")
 puts $?.exitstatus
-system("ping 192.168.16.16 -c 1 > /dev/null")
+puts system("ping 192.168.16.16 -c 1 > /dev/null")
 puts $?.exitstatus
