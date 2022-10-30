02249d40e3ecb6ddf693c457c50cd596ff30af1c
dvarrui <dvarrui@protonmail.com>
2022-10-30 16:31:22 +0000

    [N] .

---
+++ examples/comparative/examples/051-sumar.s
@@ -0,0 +1,39 @@
+	.file	"051-sumar.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Sum of %d and %d is: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$3, -4(%rbp)
+	movl	$4, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (SUSE Linux) 12.2.1 20221020 [revision 0aaef83351473e8f4eb774f8f999bbe87a4866d7]"
+	.section	.note.GNU-stack,"",@progbits
