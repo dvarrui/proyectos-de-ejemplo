53ba51a678ba0a7e2751fb627e5fcb67087f2058
dvarrui <dvarrui@protonmail.com>
2022-11-03 21:17:22 +0000

    [U] .

--- examples/elixir/05.compile/02-clause-beam.exs
+++ examples/elixir/05.compile/02-clause-beam.exs
@@ -13,12 +13,12 @@
 # require Foo
 #
 # IMPORTANTE!!!: elixirc lib/clause-lib.exs
-require Clause
+require Klause
 
 IO.puts "[ info ] odd? 1, 2"
-IO.puts Clause.is_odd 1
-IO.puts Clause.is_odd 2
+IO.puts Klause.is_odd 1
+IO.puts Klause.is_odd 2
 
 IO.puts "[ info ] even? 1, 2"
-IO.puts Clause.is_even 1
-IO.puts Clause.is_even 2
+IO.puts Klause.is_even 1
+IO.puts Klause.is_even 2
--- examples/elixir/README.md
+++ examples/elixir/README.md
@@ -9,7 +9,6 @@ Enlaces de interés:
 * https://elixir-lang.org/
 * https://samuelmullen.com/articles/elixir-processes-send-and-receive/
 
-
 dvarrui, [1/3/22 2:40]
 GitHub - h4cc/awesome-elixir: A curated list of amazingly awesome Elixir and Erlang libraries, resources and shiny things. Updates:
 https://github.com/h4cc/awesome-elixir#natural-language-processing-nlp
