46b7f61d2d089fb2827de12fd63b71e8c47672c3
David Vargas <dvarrui@protonmail.com>
2022-11-09 11:05:55 +0000

    [N] .

---
+++ examples/python/05.import/04.from_ancestors/lib/greet.py
@@ -0,0 +1,13 @@
+from saludar import *
+
+def greet1(name):
+    print("Hello", name)
+
+def greet2(name):
+    print("Hello " + name)
+
+def greet3(name):
+    print("Hello %s" % name)
+
+def greet4(name):
+    print(f"Hello {name}")
---
+++ examples/python/05.import/05.same_project_no_ancestors/saludar.py
@@ -0,0 +1,11 @@
+def saludar1(name):
+    print("Hola", name)
+
+def saludar(name):
+    print("Hola " + name)
+
+def saludar3(name):
+    print("Hola %s" % name)
+
+def saludar4(name):
+    print(f"Hola {name}")
