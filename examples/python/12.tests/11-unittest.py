6b9bdad081e734d9c9b2507309812570fd7066c5
David Vargas <dvarrui@protonmail.com>
2022-11-09 09:10:48 +0000

    [N] python.

---
+++ examples/python/12.tests/11-unittest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from my_sum11 import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
+
