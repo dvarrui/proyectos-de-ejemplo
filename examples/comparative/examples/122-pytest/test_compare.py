55b557ee16fe7357eca51908181af628f026f848
David Vargas <dvarrui@protonmail.com>
2022-11-09 14:36:49 +0000

    [N] examples.

---
+++ examples/comparative/examples/122-pytest/test_compare.py
@@ -0,0 +1,15 @@
+import pytest
+@pytest.mark.great
+def test_greater():
+   num = 100
+   assert num > 100
+
+@pytest.mark.great
+def test_greater_equal():
+   num = 100
+   assert num >= 100
+
+@pytest.mark.others
+def test_less():
+   num = 100
+   assert num < 200
---
+++ examples/comparative/examples/122-pytest/test_square.py
@@ -0,0 +1,17 @@
+import pytest
+import math
+
+@pytest.mark.square
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+
+@pytest.mark.square
+def testsquare():
+   num = 7
+   assert 7*7 == 40
+
+@pytest.mark.others
+def test_equality():
+   assert 10 == 11
+
