352628abf118de74cad7db9446970d7bcca0de7b
David Vargas <dvarrui@protonmail.com>
2022-11-23 13:34:11 +0000

    [F] .

--- examples/lang.rust/07.cli/mygrep/src/lib.rs
+++ examples/lang.rust/07.cli/mygrep/src/lib.rs
@@ -2,10 +2,13 @@
 pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
-            writeln!(writer, "{}", line);
+            if let Err(e) = writeln!(writer, "{}", line) {
+                println!("Writing error: {}", e.to_string());
+            }
         }
     }
 }
+
 
 #[test]
 fn find_a_match() {
