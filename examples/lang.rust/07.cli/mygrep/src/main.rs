af08f9513204fff537dc94aa6c821cf7a7e45fde
David Vargas <dvarrui@protonmail.com>
2022-11-23 09:13:25 +0000

    [U] .

--- examples/lang.rust/07.cli/mygrep/src/main.rs
+++ examples/lang.rust/07.cli/mygrep/src/main.rs
@@ -1,8 +1,22 @@
 use std::env::args;
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
 
 fn main() {
     println!("Hello, world!");
 
-    let _pattern = args().nth(1).expect("no pattern given");
-    let _path = args().nth(2).expect("no path given");
+    let pattern = args().nth(1).expect("no pattern given");
+    let path = args().nth(2).expect("no path given");
+    let _args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
 }
