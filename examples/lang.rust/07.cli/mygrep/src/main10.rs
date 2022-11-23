aa2221c0720f457ff4908c0628a295551edc4024
David Vargas <dvarrui@protonmail.com>
2022-11-23 12:35:18 +0000

    [U] .

--- examples/lang.rust/07.cli/mygrep/src/main10.rs
+++ examples/lang.rust/07.cli/mygrep/src/main10.rs
@@ -18,7 +18,22 @@ fn main() -> Result<()> {
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    mygrep::find_matches(&content, &args.pattern, &mut std::io::stdout());
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
