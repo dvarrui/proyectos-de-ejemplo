ab4f22edd9d3c34fe5aca05212d80d3ce8100052
David Vargas <dvarrui@protonmail.com>
2022-11-24 08:30:21 +0000

    [U] .

--- examples/lang.rust/07.cli/mygrep/tests/cli.rs
+++ examples/lang.rust/07.cli/mygrep/tests/cli.rs
@@ -13,3 +13,19 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+use assert_fs::prelude::*;
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("mygrep")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
+
+    Ok(())
+}
