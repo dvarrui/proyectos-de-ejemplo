e2436431da9f41653ea367dfcbdb9e41427a09bf
dvarrui <dvarrui@protonmail.com>
2022-12-03 17:32:10 +0000

    [N] .

---
+++ examples/nim/03.control/e01_if.nim
@@ -0,0 +1,22 @@
+
+
+let
+  a = 11
+  b = 22
+  c = 999
+
+if a < b:
+  echo "a is smaller than b"
+  if 10*a < b:
+    echo "not only that, a is *much* smaller than b"
+
+if b < c:
+  echo "b is smaller than c"
+  if 10*b < c:
+    echo "not only that, b is *much* smaller than c"
+
+if a+b > c:
+  echo "a and b are larger than c"
+  if 1 < 100 and 321 > 123:
+    echo "did you know that 1 is smaller than 100?"
+    echo "and 321 is larger than 123! wow!"
