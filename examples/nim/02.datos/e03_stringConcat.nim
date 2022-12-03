502d14cd3b98106ed45c2a56ad6bbcb17452a263
dvarrui <dvarrui@protonmail.com>
2022-12-03 16:03:52 +0000

    [U] .

---
+++ examples/nim/02.datos/e03_stringConcat.nim
@@ -0,0 +1,15 @@
+var                     
+  p = "abc"
+  q = "xy"
+  r = 'z'
+
+p.add("def")
+echo "p is now: ", p
+
+q.add(r)
+echo "q is now: ", q
+
+echo "concat: ", p & q
+
+echo "p is still: ", p
+echo "q is still: ", q
