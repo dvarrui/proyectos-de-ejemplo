2ebc48c515aa564a8b6b8eae60c33cc399db9cd0
dvarrui <dvarrui@protonmail.com>
2022-12-03 16:01:40 +0000

    [N] .

---
+++ examples/nim/02.datos/relationalOperators.nim
@@ -0,0 +1,10 @@
+let
+  g = 31
+  h = 99
+
+echo "g is greater than h: ", g > h
+echo "g is smaller than h: ", g < h
+echo "g is equal to h: ", g == h
+echo "g is not equal to h: ", g != h
+echo "g is greater or equal to h: ", g >= h
+echo "g is smaller or equal to h: ", g <= h
