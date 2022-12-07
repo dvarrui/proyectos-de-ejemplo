2083661f333a45e68d91290af2549efadd08a4e4
David Vargas <dvarrui@protonmail.com>
2022-12-07 12:26:31 +0000

    [N] .

---
+++ examples/ruby/41.gems/gitlab/04-group.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require_relative "lib/gitlab"
+
+data = gitlab.group(ENV['GITLAB_GROUP_ID']).to_h
+labels = %w(id web_url visibility)
+labels.each do |label|
+  puts "#{label.rjust(15)} #{data[label]}"
+end
+puts "projects".rjust(15)
+data['projects'].each do |group|
+  puts "#{group['id'].to_s.rjust(15)} #{group['name']}"
+end
