bb84dffd29c3eba2deccc753b1e887d627f2c853
David Vargas <dvarrui@protonmail.com>
2022-12-07 13:52:41 +0000

    [N] .

--- examples/ruby/03.control/30-if-without-if.rb
+++ examples/ruby/03.control/30-if-without-if.rb
@@ -1,10 +1,30 @@
+#!/usr/bin/env ruby
 
-clas TrueClass
+class TrueClass
   def if_then
     yield
+    self
   end
 
   def else
     self
   end
 end
+
+class FalseClass
+  def if_then
+    self
+  end
+
+  def else
+    yield
+    self
+  end
+end
+
+a = 3
+( a % 2 == 0).if_then {
+  puts "#{a} is even!"
+}.else {
+  puts "#{a} is odd!"
+}
