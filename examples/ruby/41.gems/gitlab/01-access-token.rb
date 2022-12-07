730139bc57e36beef08db38ca56ca467e16920a8
David Vargas <dvarrui@protonmail.com>
2022-12-07 10:10:14 +0000

    [U] .

--- examples/ruby/41.gems/gitlab/01-access-token.rb
+++ examples/ruby/41.gems/gitlab/01-access-token.rb
@@ -8,12 +8,9 @@ Dotenv.load("./.env")
 endpoint = ENV['GITLAB_API_ENDPOINT']
 private_token = ENV['GITLAB_API_PRIVATE_TOKEN']
 
-binding.break
-
 g = Gitlab.client(
   endpoint: endpoint,
   private_token: private_token,
 )
 
-user = g.user
-puts user
+puts g.user.to_h
---
+++ examples/ruby/41.gems/gitlab/lib/gitlab.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require "dotenv"
+require "gitlab"
+
+def gitlab
+  Dotenv.load("./.env")
+  endpoint = ENV['GITLAB_API_ENDPOINT']
+  private_token = ENV['GITLAB_API_PRIVATE_TOKEN']
+
+  Gitlab.client(
+    endpoint: endpoint,
+    private_token: private_token,
+  )
+end
