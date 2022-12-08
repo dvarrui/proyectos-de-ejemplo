4411965d0f588720ed6d54f08645f11dbaf79023
David Vargas <dvarrui@protonmail.com>
2022-12-08 10:38:05 +0000

    [U] .

--- examples/ruby/42.other.languages/scripts.from.ruby/03-exec-script.rb
+++ examples/ruby/42.other.languages/scripts.from.ruby/03-exec-script.rb
@@ -12,16 +12,23 @@ class Execute
   def python(code)
     system("python -c '#{code}'")
   end
+
+  def python3(code)
+    system("python3 -c '#{code}'")
+  end
 end
 
 execute = Execute.new
 
 code = <<~CODE
- puts "Hola #{`whoami`.strip}, soy Ruby!"
+  for i in 1..3 do
+     puts i.to_s + ": Hola " + `whoami`.strip + ", soy Ruby!"
+  end
 CODE
 execute.ruby(code)
 
 code = <<~CODE
-  print("Hola, soy Python!")
+  for i in range(3):
+    print(i, ": Hola, soy Python!")
 CODE
-execute.python(code)
+execute.python3(code)
