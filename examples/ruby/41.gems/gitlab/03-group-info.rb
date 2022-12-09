08a899026d24625b13fa17cc938abb8a14c85d0b
David Vargas <dvarrui@protonmail.com>
2022-12-09 13:44:19 +0000

    [F] .

--- examples/ruby/41.gems/gitlab/03-group-info.rb
+++ examples/ruby/41.gems/gitlab/03-group-info.rb
@@ -1,13 +1,25 @@
 #!/usr/bin/env ruby
 
 require_relative "lib/gitlab"
+require "debug"
 
-data = gitlab.group(ENV['GITLAB_GROUP_ID']).to_h
-labels = %w(id web_url visibility)
-labels.each do |label|
-  puts "#{label.rjust(15)} #{data[label]}"
+data = gitlab.group(ENV['GITLAB_GROUP_ID'])
+
+if data.is_a? Gitlab::ObjectifiedHash
+  list = [ data ]
+else
+  list = data.to_a
 end
-puts "projects".rjust(15)
-data['projects'].each do |project|
-  puts "#{project['id'].to_s.rjust(15)} #{project['name']}"
+
+labels = %w(id web_url visibility)
+list.each do |item|
+  data = item.to_h
+  labels.each do |label|
+    puts "#{label.rjust(15)} #{data[label]}"
+  end
+
+  puts "projects".rjust(15)
+  data['projects'].each do |project|
+    puts "#{project['id'].to_s.rjust(15)} #{project['name']}"
+  end
 end
