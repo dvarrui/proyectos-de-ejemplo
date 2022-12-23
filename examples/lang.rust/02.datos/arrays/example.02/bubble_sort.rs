e12ba12a3cbc44763add66e1b142df18a3f56395
David Vargas <dvarrui@protonmail.com>
2022-12-23 09:02:40 +0000

    [U] .

---
+++ examples/lang.rust/02.datos/arrays/example.02/bubble_sort.rs
@@ -0,0 +1,17 @@
+// Implement a bubble sort.
+fn sort(array: &mut Vec<u32>) {
+    for i in 0..array.len() {
+        for j in 0..array.len() - i - 1 {
+            if array[j + 1] < array[j] {
+                array.swap(j, j + 1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut array = vec![3_u32, 5, 7, 9, 1, 0, 4, 8, 2, 6];
+    sort(&mut array);
+    println!("{:?}", array);
+}
+
