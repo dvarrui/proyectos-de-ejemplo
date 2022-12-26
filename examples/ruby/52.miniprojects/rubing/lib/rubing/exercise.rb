1a35bbcbb2c66c4d045c6aacab1c69f6963153e8
dvarrui <dvarrui@protonmail.com>
2022-12-26 13:31:05 +0000

    [U] .

--- examples/ruby/52.miniprojects/rubing/lib/rubing/exercise.rb
+++ examples/ruby/52.miniprojects/rubing/lib/rubing/exercise.rb
@@ -1,3 +1,5 @@
+require "colorize"
+
 class Exercise
   attr_reader :filename
 
@@ -14,23 +16,21 @@ class Exercise
 
   def call
     puts "\n" + "_" * 50
-    puts "=> Exercise: #{filename}\n\n"
+    puts "=> Exercise: #{filename}\n".colorize(:white)
     syntax_ok = system("ruby -c #{filename}")
     if syntax_ok
-      puts "=> Syntax: OK"
-      puts "\n=> Output:"
+      puts "\n=> Output:".colorize(:white)
       exec_ok = system("ruby #{filename}")
-    else
-      puts "=> Syntax: Revise!"
     end
 
     unless @todo.size.zero?
       lines = @content.split("\n")
+      puts "\n=> TODO lines:".colorize(:white)
       @todo.each do |index|
-        puts "%02d | %s" % [index + 1, lines[index]]
+        puts ("%02d | %s" % [index + 1, lines[index]]).colorize(:cyan)
       end
     else syntax_ok and exec_ok
-      puts "\n=> Delete 'I AM NOT DONE' to continue!"
+      puts "\n=> Delete 'I AM NOT DONE' to continue!".colorize(:cyan)
     end
   end
 
