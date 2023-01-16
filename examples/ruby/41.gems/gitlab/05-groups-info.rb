eeb340261722dec56374beb697823a75b4888bdc
David Vargas <dvarrui@protonmail.com>
2023-01-16 14:21:29 +0000

    U

--- examples/ruby/41.gems/gitlab/05-groups-info.rb
+++ examples/ruby/41.gems/gitlab/05-groups-info.rb
@@ -3,10 +3,12 @@ require_relative "lib/gitlab"
 
 groups = gitlab.groups
 
-labels = %w(id name web_url name parent_id)
+labels = %w(id name web_url full_path parent_id)
 groups.each do |group|
   labels.each do |label|
     puts "#{label.rjust(18)}: #{group[label]}"
   end
   puts ""
 end
+
+puts groups[0].to_h
