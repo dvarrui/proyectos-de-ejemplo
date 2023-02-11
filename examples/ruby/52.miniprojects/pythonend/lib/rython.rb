3694cd955f7cfb93bef56a46f5c9cb524debf07b
dvarrui <dvarrui@protonmail.com>
2023-02-11 23:40:36 +0000

    N

--- examples/ruby/52.miniprojects/pythonend/lib/rython.rb
+++ examples/ruby/52.miniprojects/pythonend/lib/rython.rb
@@ -5,6 +5,10 @@ end
 
 def type(x)
   key = x.class.to_s.to_sym
-  types = { String: "str", Integer: "int"}
+  types = { String: "str", Integer: "int", Float: "float"}
   "<type '#{types[key]}'>"
 end
+
+def str(obj) = obj.to_s
+def int(obj) = obj.to_i
+def float(obj) = obj.to_f
