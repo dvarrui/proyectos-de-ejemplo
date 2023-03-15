ed031b0e2175f94ccf31740b1f41ad86316e1c9b
David Vargas <dvarrui@protonmail.com>
2023-03-15 10:07:16 +0000

    D

--- examples/lang.rust/04.use_crate/target/debug/deps/ferris_says-68f3bdfe949f4025.d
+++
@@ -1,7 +0,0 @@
-/home/quigon/proy/repos/lang.d/proyectos-de-ejemplo/examples/lang.rust/04.use_crate/target/debug/deps/ferris_says-68f3bdfe949f4025.rmeta: /home/quigon/.cargo/registry/src/github.com-1ecc6299db9ec823/ferris-says-0.2.1/src/lib.rs
-
-/home/quigon/proy/repos/lang.d/proyectos-de-ejemplo/examples/lang.rust/04.use_crate/target/debug/deps/libferris_says-68f3bdfe949f4025.rlib: /home/quigon/.cargo/registry/src/github.com-1ecc6299db9ec823/ferris-says-0.2.1/src/lib.rs
-
-/home/quigon/proy/repos/lang.d/proyectos-de-ejemplo/examples/lang.rust/04.use_crate/target/debug/deps/ferris_says-68f3bdfe949f4025.d: /home/quigon/.cargo/registry/src/github.com-1ecc6299db9ec823/ferris-says-0.2.1/src/lib.rs
-
-/home/quigon/.cargo/registry/src/github.com-1ecc6299db9ec823/ferris-says-0.2.1/src/lib.rs:
