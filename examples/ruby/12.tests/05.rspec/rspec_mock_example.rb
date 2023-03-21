bd95475a9e830e99f592632879d21ff24696e6fe
dvarrui <dvarrui@protonmail.com>
2023-03-21 20:26:46 +0000

    N rspec mock example

---
+++ examples/ruby/12.tests/05.rspec/rspec_mock_example.rb
@@ -0,0 +1,11 @@
+require "rspec"
+require_relative "image_fliper"
+
+RSpec.describe "ImageFlipper" do
+  it "calls the flip method with the correct arguments" do
+    mock = double("mini_magick")
+    expect(mock).to receive(:flip).with("ruby.jpg")
+    img = ImageFlipper.new(mock)
+    img.flip("ruby.jpg")
+  end
+end
