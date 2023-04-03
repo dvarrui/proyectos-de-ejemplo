7f5eb505b33f847621793a0b1870f288d864b133
David Vargas <dvarrui@protonmail.com>
2023-04-03 15:09:06 +0100

    N files hs

---
+++ examples/haskell/01.hello/20-repl.hs
@@ -0,0 +1,11 @@
+main = do
+    putStr "? "
+    line <- getLine
+    if null line
+        then return ()
+        else do
+            putStrLn $ reverseWords line
+            main
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
