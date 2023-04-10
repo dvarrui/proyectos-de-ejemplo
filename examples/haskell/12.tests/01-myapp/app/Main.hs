429cbc3d8987950abfe08d429c4cccc176c8e237
dvarrui <dvarrui@protonmail.com>
2023-04-10 17:41:58 +0100

    U

--- examples/haskell/12.tests/01-myapp/app/Main.hs
+++ examples/haskell/12.tests/01-myapp/app/Main.hs
@@ -1,4 +1,7 @@
 module Main where
 
+import HaskellSay (haskellSay)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  haskellSay "Hello, Haskell! You're using a function from another package!"
