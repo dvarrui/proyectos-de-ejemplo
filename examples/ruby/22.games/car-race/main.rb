8b2821f92a97082156014e7fbd4e55232996fc6e
dvarrui <dvarrui@protonmail.com>
2023-05-21 11:04:39 +0100

    U refactoring new World class

--- examples/ruby/22.games/car-race/main.rb
+++ examples/ruby/22.games/car-race/main.rb
@@ -2,25 +2,17 @@
 
 require "colorize"
 require_relative "screen"
+require_relative "world"
 
 trap('SIGINT') { puts "Bye!"; exit! }
 
 screen = Screen.new
-road_positions = Array.new(screen.height, screen.width/2 - 5)
-colors = %I(red green blue magenta cyan)
+world = World.new(screen)
 
 screen.clear
 loop do
-  new_road_position = road_positions.last + 1 - rand(3)
-  if new_road_position < 10 || new_road_position > screen.width - 10
-    new_road_position = road_positions.last
-  end
-  road_positions.push(new_road_position)
-  road_positions.shift
-
-  road_positions.reverse.each_with_index do |x, y|
-    screen.show x, y, "#          #".cyan
-  end
+  world.update
+  world.render
   screen.move_top_left
   sleep 0.1
 end
