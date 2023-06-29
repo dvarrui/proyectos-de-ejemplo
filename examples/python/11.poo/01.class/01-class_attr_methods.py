216b31f922486719ead478433e7a7d8553ec4911
David Vargas <dvarrui@protonmail.com>
2023-06-29 09:58:06 +0100

    [U]

--- examples/python/11.poo/01.class/01-class_attr_methods.py
+++ examples/python/11.poo/01.class/01-class_attr_methods.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 
 class Person:
@@ -6,12 +7,14 @@ class Person:
         self.age = age
 
     def greet(self):
-        print "Hello! I am " + self.name
+        print("Hello! I am " + self.name)
 
     # Ruby use to_s method name
     def __str__(self):
         return "Name: " + self.name + ", Age: " + str(self.age)
 
-p = Person('Tomas', 44) # Ruby call new method to build instance
+p = Person('Yoda', 900) # Ruby call new method to build instance
 p.greet()
-print p # Call __str__ method to convert object to String
+print(p)                # Python call __str__ method to convert object to String
+
+Person('Obiwan', 53).greet()
