feee9d68351159372c8f9c24fb409d26034da8ae
David Vargas <dvarrui@protonmail.com>
2023-07-25 12:00:10 +0100

    [U]

--- examples/ruby/31.network/23.selenium/11-unide.rb
+++ examples/ruby/31.network/23.selenium/11-unide.rb
@@ -13,19 +13,40 @@ driver.get url
 puts "[Title] #{driver.title}"
 index = 0
 
-places = driver.find_elements(xpath: '//div[@class="pol-12"]')
+places = driver.find_elements(xpath: '//div[@class="sl-addr-sec"]')
 places.each do |div|
   index += 1
   h = div.find_element(xpath: 'descendant::h3')
-  puts format("%3d", index) + " " + h.text
+
   addr = div.find_element(xpath: 'descendant::li[@class="sl-addr"]/span').text
+  addrs = addr.split("\n")
+  cod_postal = addrs[1].split(",").last.strip
+  puts "#{index} #{cod_postal}"
+  next unless cod_postal.start_with?("35", "38") && cod_postal.size == 5
+
   phone = div.find_element(xpath: 'descendant::li[@class="sl-phone"]/a').text
   email = div.find_element(xpath: 'descendant::li[@class="sl-email"]/a').text
-  days = div.find_element(xpath: 'descendant::span[@class="asl-day-lbl"]').text
-  puts "     addr: #{addr.gsub("\n", " - ")}"
-  puts "    phone: #{phone}"
-  puts "    email: #{email}"
-  puts "     days: #{days}"
+  begin
+    days = div.find_element(xpath: 'descendant::span[@class="asl-day-lbl"]').text
+    hours = div.find_element(xpath: 'descendant::span[@class="asl-time-hrs"]').text
+    days2 = div.find_element(xpath: 'descendant::span[@class="txt-hours"]').text
+  rescue
+    days = ""
+    hours = ""
+    days2 = ""
+  end
+  category = div.find_element(xpath: 'descendant::li[@class="sl-categories"]/ul/li/span').text
+
+  puts format("%3d", index) + " " + h.text
+  puts "          addr0: #{addrs[0]}"
+  puts "          addr1: #{addrs[1]}"
+  puts "     cod_postal: #{cod_postal}"
+  puts "          phone: #{phone}"
+  puts "          email: #{email}"
+  puts "           days: #{days}"
+  puts "          hours: #{hours.gsub("\n", ", ")}"
+  puts "          days2: #{days2.gsub("\n", ", ")}"
+  puts "       category: #{category}"
 
   # h.find_elements(xpath: "descendant::a").each do |a|
   #   puts "      #{a.text}"
