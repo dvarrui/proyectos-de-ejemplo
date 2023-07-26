dc2d9efe951b5e02776b62c57de91b01b47b5fd0
David Vargas <dvarrui@protonmail.com>
2023-07-26 14:17:42 +0100

    [U]

--- examples/ruby/31.network/23.selenium/10-aldi.rb
+++ examples/ruby/31.network/23.selenium/10-aldi.rb
@@ -1,10 +1,48 @@
 #!/usr/bin/env ruby
 
-require_relative "lib/selenium-wrapper"
+require "colorize"
+require "selenium-webdriver"
 
-URL = "https://www.aldi.es/supermercados/encuentra-tu-supermercado.html/s?q=Canarias%2C%20Espa%C3%B1a"
+def pause
+  puts "(Push enter to continue)".colorize(:light_yellow)
+  gets
+end
 
-browser = SeleniumWrapper.new(URL)
-driver = browser.driver
+options = Selenium::WebDriver::Firefox::Options.new
+# options.add_argument("-headless")
+driver = Selenium::WebDriver.for :firefox, options: options
 
-browser.quit
+# https://www.aldi.es/supermercados/encuentra-tu-supermercado.html
+# url = "https://www.aldi.es/supermercados/encuentra-tu-supermercado.html/s?q=Canarias%2C%20Espa%C3%B1a"
+url = "https://www.aldi.es/supermercados/encuentra-tu-supermercado.html"
+driver.manage.timeouts.implicit_wait = 30
+driver.get url
+puts "[Title] #{driver.title}"
+
+sleep 10
+driver.find_elements(:tag_name, 'button').each do |button|
+  puts button.text.strip.upcase
+  if button.text.strip.upcase == "ACEPTAR"
+    puts "==> click button ACEPTAR"
+    button.click
+  end
+  if button.text.strip.upcase == "SALTAR"
+    puts "==> click button SALTAR"
+    button.click
+  end
+end
+pause
+puts "==> send_keys"
+driver.find_element(xpath: '//input').send_keys "Canarias, España", :return
+pause
+
+handles = driver.window_handles
+puts handles
+
+places = driver.find_elements(xpath: '//div[starts-with(@class,"ubsf")]')
+places.each_with_index do |div, index|
+  puts "#{index} #{div.text}"
+end
+
+driver.quit
+exit 0
