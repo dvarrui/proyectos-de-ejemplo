3c1ca3b1cf730086e19faeafed7eb47ca8007e41
dvarrui <dvarrui@protonmail.com>
2023-08-13 00:26:29 +0100

    [U] split into several classes

---
+++ examples/ruby/22.games/defrag/lib/progress.rb
@@ -0,0 +1,19 @@
+
+class Progress
+  attr_accessor :screen
+
+  def init
+    @duration = 0.0
+  end
+
+  def update(delta)
+    @duration += delta
+    @min = format("%02d", @duration/60).on_blue
+    @seg = format("%02d", (@duration - (@duration/60).to_i * 60)).on_blue
+  end
+
+  def render
+    @screen.show 28, @screen.height - 3, @min
+    @screen.show 31, @screen.height - 3, @seg
+  end
+end
--- examples/ruby/22.games/defrag/scene.rb
+++ examples/ruby/22.games/defrag/scene.rb
@@ -1,8 +1,10 @@
+require_relative "lib/panel"
+require_relative "lib/progress"
+
 class Scene
   attr_accessor :screen
 
   def init
-    @panel = File.read("panel.txt").on_blue
     @symbol = {
       space: " ".on_blue,
       # block: " ".on_green,
@@ -14,46 +16,25 @@ class Scene
       bad: "B".light_white.on_blue,
       unmovable: "X".light_white.on_blue,
     }
-    @state = :init
-    @duration = 0.0
+    @panel = Panel.new
+    @panel.screen = @screen
+    @panel.symbol = @symbol
+    @panel.init
+    @progress = Progress.new
+    @progress.screen = @screen
+    @progress.init
   end
 
   def update(delta)
-    @duration += delta
-    @min = format("%02d", @duration/60).on_blue
-    @seg = format("%02d", (@duration - (@duration/60).to_i * 60)).on_blue
+    @panel.update(delta)
+    @progress.update(delta)
   end
 
   def render
-    if @state == :init
-      fill_screen
-      show_panel
-      @state = :running
-    end
+    @panel.render
+    @progress.render
     @symbol.values.each_with_index do |s, index|
       @screen.show 2+index, 2, s
     end
-    @screen.show 28, @screen.height - 3, @min
-    @screen.show 31, @screen.height - 3, @seg
-  end
-
-  private
-
-  def fill_screen
-    (1..@screen.height).each do |y|
-      (1..@screen.width).each do |x|
-        @screen.show x, y, @symbol[:space]
-      end
-    end
-  end
-
-  def show_panel
-    y = @screen.height - 6
-    x = 45
-    @screen.show 1, y, @panel
-    @screen.show x, y + 1, @symbol[:used2]
-    @screen.show x + 17, y + 1, @symbol[:unused]
-    @screen.show 17, y, " Status ".light_yellow.on_blue
-    @screen.show 57, y, " Legend ".light_yellow.on_blue
   end
 end
