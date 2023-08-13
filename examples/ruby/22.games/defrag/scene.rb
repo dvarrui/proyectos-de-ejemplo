4bbb83c8788eceb1386511a9bd1afac414c485a6
dvarrui <dvarrui@protonmail.com>
2023-08-13 11:37:47 +0100

    [N] class Disk

--- examples/ruby/22.games/defrag/scene.rb
+++ examples/ruby/22.games/defrag/scene.rb
@@ -1,3 +1,4 @@
+require_relative "lib/disk"
 require_relative "lib/panel"
 require_relative "lib/progress"
 
@@ -11,25 +12,25 @@ class Scene
       used1: "\u25cf".blue.on_light_yellow,
       used2: "\u25cf".blue.on_light_white,
       unused: " ".on_light_black, # white,
-      reading: "R".light_white.on_blue,
+      reading: "r".light_white.on_blue,
       writting: "W".light_white.on_blue,
       bad: "B".light_white.on_blue,
       unmovable: "X".light_white.on_blue,
     }
     @panel = Panel.new(@screen, @symbol)
+    @disk = Disk.new(@screen, @symbol)
     @progress = Progress.new(@screen, @symbol)
   end
 
   def update(delta)
     @panel.update(delta)
+    @disk.update(delta)
     @progress.update(delta)
   end
 
   def render
     @panel.render
+    @disk.render
     @progress.render
-    @symbol.values.each_with_index do |s, index|
-      @screen.show 2+index, 2, s
-    end
   end
 end
