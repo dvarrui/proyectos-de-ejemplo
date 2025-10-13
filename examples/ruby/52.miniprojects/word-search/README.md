59461b2d81c82d81f2a934a2836048805e6452a9
David Vargas <dvarrui@protonmail.com>
2025-10-13 21:58:57 +0100

    Move files to their own repo

--- examples/ruby/52.miniprojects/word-search/README.md
+++
@@ -1,4 +0,0 @@
-
-# Word Search - Sopa de letras
-
-Vamos a intentar hacer un script ruby para crear sopas de letras.
--- examples/ruby/52.miniprojects/word-search/lib/location.rb
+++
@@ -1,9 +0,0 @@
-class Location
-  attr_reader :row, :col, :data
-
-  def initialize(row, col, data)
-    @row = row
-    @col = col
-    @data = @data
-  end
-end
