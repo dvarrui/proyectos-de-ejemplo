0cbbf5cf052906542d5b3eb98b5bd85a22e3824e
David Vargas <dvarrui@protonmail.com>
2026-01-10 13:00:04 +0000

    new example

---
+++ examples/ruby/51.ejecicios/11.first_or_last/first_or_last_test.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+require 'test/unit'
+require_relative 'first_or_last'
+
+class TestFactorial < Test::Unit::TestCase
+  def setup
+    @examples = [
+      { text: "Hola Caracola!", first: "o", last: "a" },
+      { text: "HOlA CaracolA!", first: "a", last: "o" },
+      { text: "Pienso que sólo sé que no sé nada.", first: "i", last: "a" },
+      { text: "PIenso que sólo sé que no sé nAdA.", first: "e", last: "o" },
+    ]
+  end
+
+  def test_first
+    @examples.each do |example|
+      assert_equal(example[:first], first_or_last(example[:text], true))
+    end
+  end
+
+  def test_last
+    @examples.each do |example|
+      assert_equal(example[:last], first_or_last(example[:text], false))
+    end
+  end
+end
