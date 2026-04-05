3368f7539d56ee968fd49df7abbf5c4bda2c656d
David Vargas <dvarrui@protonmail.com>
2026-04-05 11:38:00 +0100

    update

--- projects/comparar/add-two-numbers/01-ruby.rb
+++ projects/comparar/add-two-numbers/01-ruby.rb
@@ -3,8 +3,5 @@
 num1 = 1.5
 num2 = 6.3
 
-# Add two numbers
 sum = num1 + num2
-
-# Display the sum
 puts "The sum of #{num1} and #{num2} is #{sum}"
--- projects/comparar/add-two-numbers/02-ruby.rb
+++ projects/comparar/add-two-numbers/02-ruby.rb
@@ -4,8 +4,6 @@ num1 = gets.to_f
 print "Enter second number: "
 num2 = gets.to_f
 
-# Add two numbers
 sum = num1 + num2
 
-# Display the sum
 puts "The sum of #{num1} and #{num2} is #{sum}"
