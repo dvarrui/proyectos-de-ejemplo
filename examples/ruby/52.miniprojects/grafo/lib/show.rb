1e384be3423ba05ef9f884e4c9db5b45d822ead0
David Vargas <dvarrui@protonmail.com>
2026-04-16 12:21:08 +0100

    update show

--- examples/ruby/52.miniprojects/grafo/lib/show.rb
+++ examples/ruby/52.miniprojects/grafo/lib/show.rb
@@ -2,12 +2,12 @@
 class Graph
   def show
     puts "Graph (filename: #{@filename})"
-    puts " | nodes (#{@nodes.size})"
-    puts " | arcs  (directed? #{directed?})"
+    puts "  > nodes (#{@nodes.size})"
+    puts "  > arcs  #{(directed?() ? '(directed)' : '')}" 
     @arcs.each do |arc|
       arrow = "--"
       arrow = "->" if directed?
-      puts "    #{arc[0]} #{arrow} #{arc[1]}"
+      puts "    | #{arc[0]} #{arrow} #{arc[1]}"
     end
   end
 end
